7da3589c408e8436a6eaff142a42c7447e6d7067
Kirby Olson <riknos314@gmail.com>
2015-07-10 09:13:37 -0500

    preparing tabbed for PR

--- runestone/tabbedStuff/tabbedStuff.py
+++ runestone/tabbedStuff/tabbedStuff.py
@@ -13,121 +13,86 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__author__ = 'isaacdontjelindell'
+__author__ = 'isaiahmayerchak'
 
 from docutils import nodes
 from docutils.parsers.rst import directives
 from docutils.parsers.rst import Directive
 
-
 def setup(app):
+#Add directives/javascript/css
     app.add_directive('tabbed', TabbedStuffDirective)
     app.add_directive('tab', TabDirective)
 
     app.add_node(TabNode, html=(visit_tab_node, depart_tab_node))
     app.add_node(TabbedStuffNode, html=(visit_tabbedstuff_node, depart_tabbedstuff_node))
 
+    app.add_javascript('tabbedstuff.js')
+
     app.add_stylesheet('tabbedstuff.css')
 
+#Templates to be formatted by node options
+BEGIN = """<div id='%(divid)s' data-component="tabbedStuff" %(inactive)s>"""
 
-BEGIN = """<div id='%(divid)s' class='alert alert-warning'>"""
-
-TABLIST_BEGIN = """<ul class='nav nav-tabs' id='%(divid)s_tab'>"""
-
-TABLIST_ELEMENT = """
-<li>
-    <a data-toggle='tab' href='#%(divid)s-%(tabname)s'><span>%(tabfriendlyname)s</span></a>
-</li>
+TABDIV_BEGIN = """<div data-component="tab" data-tabname="%(tabname)s" %(active)s>
 """
 
-TABLIST_END = """</ul>"""
-
-TABCONTENT_BEGIN = """<div class='tab-content'>"""
-TABCONTENT_END = """</div>"""
-
-TABDIV_BEGIN = """<div class='tab-pane' id='%(divid)s-%(tabname)s'>"""
-
-TABDIV_END = """</div>""" 
+TABDIV_END = """</div>"""
 
 END = """
     </div>
-    <script type='text/javascript'>
-        $('#%(divid)s .nav-tabs a').click(function (e) {
-            e.preventDefault();
-            $(this).tab('show');
-        })
 
-        // activate the first tab
-        var el = $('#%(divid)s .nav-tabs a')[0];
-        $(el).tab('show');
-
-        $('#%(divid)s .nav-tabs a').on('shown.bs.tab', function (e) {
-            var content_div = $(e.target.attributes.href.value);
-            content_div.find('.disqus_thread_link').each(function() {
-                $(this).click();
-            });
-
-            content_div.find('.CodeMirror').each(function(i, el) {
-                el.CodeMirror.refresh();
-            });
-        })
-    </script>
 """
 
 class TabNode(nodes.General, nodes.Element):
     def __init__(self, content):
         super(TabNode, self).__init__()
-        self.tabnode_components = content
+        self.tabnode_options = content
         self.tabname = content['tabname']
 
 def visit_tab_node(self, node):
-    divid = node.parent.divid 
+#Set options and format templates accordingly
+    divid = node.parent.divid
     tabname = node.tabname
 
-    # remove spaces from tabname to allow it to be used as the div id.
+    if 'active' in node.tabnode_options:
+        node.tabnode_options['active'] = 'data-active'
+    else:
+        node.tabnode_options['active'] = ''
+
     res = TABDIV_BEGIN % {'divid':divid,
-                          'tabname':tabname.replace(" ", "")}
+                          'tabname':tabname,
+                          'active': node.tabnode_options['active']}
     self.body.append(res)
 
 def depart_tab_node(self,node):
+#Set options and format templates accordingly
     self.body.append(TABDIV_END)
 
 class TabbedStuffNode(nodes.General, nodes.Element):
     '''A TabbedStuffNode contains one or more TabNodes'''
     def __init__(self,content):
         super(TabbedStuffNode,self).__init__()
-        self.tabbed_stuff_components = content
+        self.tabbed_stuff_options = content
         self.divid = content['divid']
 
 def visit_tabbedstuff_node(self, node):
     divid = node.divid
+    if 'inactive' in node.tabbed_stuff_options:
+        node.tabbed_stuff_options['inactive'] = 'data-inactive'
+    else:
+        node.tabbed_stuff_options['inactive'] = ''
 
-    # this is all the child tab nodes
-    tabs = node.traverse(include_self=False, descend=True, condition=TabNode) 
-
-    res = BEGIN % {'divid':divid}
-    res += TABLIST_BEGIN
-    
-    # make the tab list (<ul>).
-    # tabfriendlyname can contain spaces and will be displayed as the name of the tab.
-    # tabname is the same as tabfriendlyname but with spaces removed, so it can be 
-    # used as the div id.
-    for tab in tabs:
-        res += TABLIST_ELEMENT % {'divid':divid,
-                                  'tabfriendlyname':tab.tabname,  
-                                  'tabname':tab.tabname.replace(" ", "")}  
-
-    res += TABLIST_END  # </ul>
-    res += TABCONTENT_BEGIN
+    res = BEGIN % {'divid':divid,
+                    'inactive':node.tabbed_stuff_options['inactive']}
 
     self.body.append(res)
 
 
 def depart_tabbedstuff_node(self,node):
     divid = node.divid
-
+    res = ""
     # close the tab plugin div and init the Bootstrap tabs
-    res = TABCONTENT_END
     res += END
 
     res = res % {'divid':divid}
@@ -141,14 +106,24 @@ class TabDirective(Directive):
     optional_arguments = 0
     final_argument_whitespace = True
     has_content = True
-    option_spec = {}
+    option_spec = {'active':directives.flag}
 
     node_class = TabNode
 
     def run(self):
+        """
+            process the tab directive and generate html for output.
+            :param self:
+            :return:
+            .. tab:: identifier
+            :active: Optional flag that specifies this tab to be opened when page is loaded (default is first tab)--overridden by :inactive: flag on tabbedStuff
+
+            Content
+            ...
+            """
         # Raise an error if the directive does not have contents.
         self.assert_has_content()
-        
+
         # Create the node, to be populated by "nested_parse".
         self.options['tabname'] = self.arguments[0]
         tab_node = TabNode(self.options)
@@ -162,10 +137,23 @@ class TabbedStuffDirective(Directive):
     optional_arguments = 0
     final_argument_whitespace = True
     has_content = True
+    option_spec = {'inactive':directives.flag}
 
     def run(self):
+        """
+            process the tabbedStuff directive and generate html for output.
+            :param self:
+            :return:
+            .. tabbed:: identifier
+            :inactive: Optional flag that calls for no tabs to be open on page load
+
+            Content (put tabs here)
+            ...
+            """
+
         # Raise an error if the directive does not have contents.
         self.assert_has_content()
+
 
         self.options['divid'] = self.arguments[0]
 
@@ -175,4 +163,3 @@ class TabbedStuffDirective(Directive):
         # Parse the directive contents (should be 1 or more tab directives)
         self.state.nested_parse(self.content, self.content_offset, tabbedstuff_node)
         return [tabbedstuff_node]
-
