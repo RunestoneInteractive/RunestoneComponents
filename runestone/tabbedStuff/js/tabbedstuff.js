1b28e946359ef859a53b6aa355989b55c8d72ed3
Kirby Olson <riknos314@gmail.com>
2015-07-13 11:21:28 -0500

    removed runestoneBase declaration

--- runestone/tabbedStuff/js/tabbedstuff.js
+++ runestone/tabbedStuff/js/tabbedstuff.js
@@ -8,17 +8,6 @@
 ===           Isaiah Mayerchak           ===
 ===               06/15/15               ===
 ==========================================*/
-function RunestoneBase () {    // Parent function
-
-}
-
-RunestoneBase.prototype.logBookEvent = function (info) {
-    console.log("logging event " + this.divid);
-};
-
-RunestoneBase.prototype.logRunEvent = function (info) {
-    console.log("running " + this.divid);
-};
 
 var TSList = {};    // Dictionary that contains all instances of TabbedStuff objects
 
