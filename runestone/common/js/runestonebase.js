e437c09ef3fce1fe18d90d3ffa4ef20636b05a57
Brad Miller <bonelake@mac.com>
2015-07-24 15:36:17 -0500

    When services are false do not attempt ajax

--- runestone/common/js/runestonebase.js
+++ runestone/common/js/runestonebase.js
@@ -4,7 +4,7 @@ function RunestoneBase () {   // Basic parent stuff
 
 RunestoneBase.prototype.logBookEvent = function (eventInfo) {
     eventInfo.course = eBookConfig.course;
-    if (eBookConfig.logLevel > 0) {
+    if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
         jQuery.get(eBookConfig.ajaxURL + 'hsblog', eventInfo); // Log the run event
     }
     console.log("logging event " + eventInfo);
@@ -12,7 +12,7 @@ RunestoneBase.prototype.logBookEvent = function (eventInfo) {
 
 RunestoneBase.prototype.logRunEvent = function (eventInfo) {
     eventInfo.course = eBookConfig.course;
-    if (eBookConfig.logLevel > 0) {
+    if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
         jQuery.post(eBookConfig.ajaxURL + 'runlog', eventInfo); // Log the run event
     }
     console.log("running " + eventInfo);
