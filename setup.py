6890138664fed7c05c60fe8ac613e63301ce5abd
Brad Miller <bonelake@mac.com>
2016-05-29 10:33:50 -0500

    bug fix, construct dburl from environ if possible

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as fh:
 setup(
     name='runestone',
     description='Sphinx extensions for writing interactive documents.',
-    version='2.2.0',
+    version='2.2.1',
     author = 'Brad Miller',
     author_email = 'bonelake@mac.com',
     packages= find_packages(),
@@ -16,8 +16,8 @@ setup(
     package_dir = {'runestone' : 'runestone'},
     package_data = { '' : ['js/*.js', 'css/*.css', '*.txt']},
     license='GPL',
-    url = 'https://github.com/RunestoneInteractive/RunestoneTools',
-    download_url = 'https://github.com/RunestoneInteractive/RunestoneTools/tarball/2.1.6',
+    url = 'https://github.com/RunestoneInteractive/RunestoneComponents',
+    download_url = 'https://github.com/RunestoneInteractive/RunestoneComponents/tarball/2.2.1',
     keywords = ['runestone', 'sphinx', 'ebook'], # arbitrary keywords
     classifiers=('Development Status :: 5 - Production/Stable',
                    'Environment :: Console',
