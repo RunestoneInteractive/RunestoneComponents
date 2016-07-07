d40e7d1302c55858bcc90e017f7ff1fc117d6b53
Brad Miller <bonelake@mac.com>
2016-07-07 10:34:11 -0500

    Add History Slider test

--- runestone/activecode/test/test_activecode.py
+++ runestone/activecode/test/test_activecode.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.common.exceptions import WebDriverException
 import unittest
 
@@ -8,11 +9,12 @@ class ActiveCodeTests(unittest.TestCase):
         self.driver = webdriver.Firefox()
         self.host = 'http://127.0.0.1:8080'
 
-    def runTest(self):
+    def test_hello(self):
         '''
-        1. Get a list of all the chapters/pages in all the courses specified in self.courses_to_test
-        2. Find and run every ActiveCode block on each page
-        3. Print out information about any ActiveCode that results in an error.
+        1. Get the outer div id of the activecode component
+        2. Find the run button using its class name
+        3. Run the example
+        4. Check the output from the ac_output element
         :return:
         '''
         self.driver.get(self.host + "/index.html")
@@ -24,6 +26,36 @@ class ActiveCodeTests(unittest.TestCase):
         output = t1.find_element_by_class_name("ac_output")
         self.assertEqual(output.text.strip(),"Hello World")
 
-    def tearDown(self):
-        self.driver.quit()
+
+    def test_history(self):
+        '''
+        1. Get the outer div id of the activecode component
+        2. Find the run button using its class name
+        3. Run the example
+        4. Check the output from the ac_output element
+        :return:
+        '''
+        self.driver.get(self.host + "/index.html")
+        t1 = self.driver.find_element_by_id("test1")
+        self.assertIsNotNone(t1)
+        rb = t1.find_element_by_class_name("run-button")
+        self.assertIsNotNone(rb)
+        rb.click()
+        ta = t1.find_element_by_class_name("cm-s-default")
+        self.assertIsNotNone(ta)
+        self.driver.execute_script("""edList['test1'].editor.setValue("print('GoodBye')")""")
+        rb.click()
+        output = t1.find_element_by_class_name("ac_output")
+        self.assertEqual(output.text.strip(),"GoodBye")
+        move = ActionChains(self.driver)
+        slider = t1.find_element_by_class_name("ui-slider")
+        width = slider.size['width']
+        slider = t1.find_element_by_class_name("ui-slider-handle")
+        move.click_and_hold(slider).move_by_offset(-width,0).release().perform()
+        rb.click()
+        output = t1.find_element_by_class_name("ac_output")
+        self.assertEqual(output.text.strip(), "Hello World")
+
+#    def tearDown(self):
+#        self.driver.quit()
 
