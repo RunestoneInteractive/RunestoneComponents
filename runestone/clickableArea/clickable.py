3e9640ff2297d6b3be2c79b59b6600ea9038722a
Brad Miller <bonelake@mac.com>
2016-07-14 13:40:28 -0500

    Update doc string

--- runestone/clickableArea/clickable.py
+++ runestone/clickableArea/clickable.py
@@ -77,13 +77,16 @@ def depart_ca_node(self,node):
 
 class ClickableArea(Directive):
     """
-.. clickablearea:: uniqueid
-   "question":directives.unchanged,
-   "feedback":directives.unchanged,
-   "iscode":directives.flag,
-   "correct":directives.unchanged,
-   "incorrect":directives.unchanged,
-   "table":directives.flag
+.. clickablearea:: identifier
+    :question: Question text
+    :feedback: Optional feedback for incorrect answer
+    :iscode: Boolean that if present will put the content into a <pre>
+    :table: Boolean that indicates that the content is a table.
+    :correct: An array of the indices of the correct elements, separated by semicolons--if this is a table, each item in the array is a tuple
+    with the first number being the row and the second number being the column--use a column number of 0 to make the whole row correct (ex: 1,2;3,0 makes the 2nd data cell in the first row correct as well as the entire 3rd row)
+    :incorrect: An array of the indices of the incorrect elements--same format as the correct elements.
+
+    --Content--
     """
     required_arguments = 1
     optional_arguments = 0
