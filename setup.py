247816272e2b0a1b337ad6482703898aae5c163b
Brad Miller <bonelake@mac.com>
2016-08-19 12:25:26 -0500

    Release: Add support for numbered sections

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as fh:
 setup(
     name='runestone',
     description='Sphinx extensions for writing interactive documents.',
-    version='2.6.6',
+    version='2.6.7',
     author = 'Brad Miller',
     author_email = 'bonelake@mac.com',
     packages= find_packages(exclude=['*.*.test']),
@@ -17,7 +17,7 @@ setup(
     package_data = { '' : ['js/*.js', 'css/*.css', '*.txt']},
     license='GPL',
     url = 'https://github.com/RunestoneInteractive/RunestoneComponents',
-    download_url = 'https://github.com/RunestoneInteractive/RunestoneComponents/tarball/2.6.6',
+    download_url = 'https://github.com/RunestoneInteractive/RunestoneComponents/tarball/2.6.7',
     keywords = ['runestone', 'sphinx', 'ebook'], # arbitrary keywords
     classifiers=('Development Status :: 5 - Production/Stable',
                    'Environment :: Console',
