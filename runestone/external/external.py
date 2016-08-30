706a94d778c5291a17845c114677eedfd2ba245f
jczetta <jczetta@gmail.com>
2016-08-30 16:51:54 -0400

    Switch to using question.py code as template for external directive. TODO: make it easier to refactor from this basis, CSS to fix formatting to be its own and static with other 'problem set' problems.

--- runestone/external/external.py
+++ runestone/external/external.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016  Bradley N. Miller
+# Copyright (C) 2011  Bradley N. Miller
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,88 +13,98 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-__author__ = 'jczetta'
-# Most of the code from question.py and shortanswer.py, (c) Bradley N. Miller.
-import os
 
 from docutils import nodes
 from docutils.parsers.rst import directives
+from runestone.common.runestonedirective import RunestoneDirective
 from docutils.parsers.rst import Directive
-from runestone.assess import Assessment
+from sqlalchemy import create_engine, Table, MetaData, select, delete
+from runestone.server import get_dburl
 from runestone.server.componentdb import addQuestionToDB, addHTMLToDB
 from runestone.common.runestonedirective import RunestoneDirective
+
 try:
     from html import escape  # py3
 except ImportError:
     from cgi import escape  # py2
 
+__author__ = 'jczetta'
+# Code template is directly from question.py at the moment, which is (c) Bradley N. Miller. 
+#This is intended as the basis for a potential new gradeable directive class, still potential TODO.
+
+
 def setup(app):
     app.add_directive('external', ExternalDirective)
+
     app.add_node(ExternalNode, html=(visit_external_node, depart_external_node))
 
-# NOTE: This external questions directive is intended for pointing students to external exercises, yet still linking the instructions to a grade. There is no autograde capability, because the activity is entiely external to the textbook. 
-# This directive and activecode by extension may be used as tpls during a refactor of a base class -> using inheritance in gradable directives. That's still a potential TODO.
-
-TEXT = """
-<p data-component="external" id=%(divid)s %(optional)s> %(content)s</p>
-"""
 
 class ExternalNode(nodes.General, nodes.Element):
-    def __init__(self, options):
+    def __init__(self, content):
         super(ExternalNode, self).__init__()
-        self.external_options = options
+        self.external_options = content
+
 
 def visit_external_node(self, node):
-    div_id = node.external_options['divid']
-    components = dict(node.external_options)
-    components.update({'divid': div_id})
-    res = TEXT % components
-    addHTMLToDB(div_id, components['basecourse'], res)
+    # Set options and format templates accordingly
+    # env = node.document.settings.env
+
+    res = TEMPLATE_START % node.external_options
+    self.body.append(res)
+
+    addHTMLToDB(node.external_options['divid'],
+                node.external_options['basecourse'],
+                "".join(""))
+
+    # self.body.remove(node.delimiter)
+
+
+def depart_external_node(self, node):
+    # Set options and format templates accordingly
+    res = TEMPLATE_END % node.external_options
+    delimiter = "_start__{}_".format(node.external_options['divid'])
 
     self.body.append(res)
 
-def depart_external_node(self,node):
-    # From activecode:
-    ''' This is called at the start of processing an activecode node.  If activecode had recursive nodes
-        etc and did not want to do all of the processing in visit_ac_node any finishing touches could be
-        added here.
+
+# Templates to be formatted by node options
+TEMPLATE_START = '''
+    <div data-component="question" class="full-width container question" id="%(divid)s" >
+    <li class="alert alert-warning">
+
     '''
-    res = TEXT % node.ac_components
-    self.body.append(res)
-
-    addHTMLToDB(node.ac_components['divid'],
-                node.ac_components['basecourse'],
-                "".join(self.body[self.body.index(node.delimiter) + 1:]))
-
-    self.body.remove(node.delimiter)
+TEMPLATE_END = '''
+    </li>
+    </div>
+    '''
 
 
 class ExternalDirective(RunestoneDirective):
     """
-.. external:: uniqueid
+.. external:: identifier
 
-   text of the question goes here
+   Content  Everything here is part of the activity
+   Content  Can include links...
     """
-    required_arguments = 1  # the div id
+    required_arguments = 1
     optional_arguments = 0
     final_argument_whitespace = True
     has_content = True
     option_spec = RunestoneDirective.option_spec.copy()
-    option_spec.update({'optional': directives.flag})
+    option_spec.update({'number': directives.positive_int})
 
-    node_class = ExternalNode
-
-    # This run method is also meant to be the basis for a class to be inherited from. We can build up from here, and continue to remove all unnecessary things.
     def run(self):
-        # Raise an error if the directive does not have contents.
-        self.assert_has_content()
         addQuestionToDB(self)
-        # keeping the optional for now in case we want to use that designation for grading
-        self.options['optional'] = 'data-optional' if 'optional' in self.options else '' # May be useful
-        self.options['divid'] = self.arguments[0]
-        self.options['content'] = "<p>".join(self.content)
-    
-        external_node = ExternalNode(self.options)
 
+        self.assert_has_content()  # make sure activity has something in it
+        self.options['divid'] = self.arguments[0]
+        self.options['basecourse'] = self.state.document.settings.env.config.html_context.get('basecourse', "unknown")
+
+        self.options['name'] = self.arguments[0].strip()
+
+        external_node = ExternalNode(self.options)
+        self.add_name(external_node)
+
+        self.state.nested_parse(self.content, self.content_offset, external_node)
 
         return [external_node]
