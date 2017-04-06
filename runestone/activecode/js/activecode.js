4be2ef3acb561d32b9a3f8f1ff2aa534eebff0a2
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2017-04-06 17:37:58 -0400

    Update audio tour UI, fix bugs, add support for multiple tours

--- runestone/activecode/js/activecode.js
+++ runestone/activecode/js/activecode.js
@@ -216,7 +216,17 @@ ActiveCode.prototype.createControls = function () {
         $(butt).css("margin-left", "10px");
         this.atButton = butt;
         ctrlDiv.appendChild(butt);
-        $(butt).click((function() {new AudioTour(this.divid, this.editor.getValue(), 1, $(this.origElem).data("audio"))}).bind(this));
+        
+        butt_temp = document.createElement("button");
+        $(butt_temp).addClass("ac_opt btn btn-default");
+        $(butt_temp).text("Stop Tour");
+        $(butt_temp).css("margin-left", "10px");
+        $(butt_temp).hide();
+        ctrlDiv.appendChild(butt_temp);
+        
+        $(butt).click((function() {
+        	new AudioTour(this.divid, this.code, 1, $(this.origElem).data("audio"));
+        }).bind(this));
     }
 
 
@@ -825,6 +835,7 @@ ActiveCode.prototype.runProg = function () {
             this.logRunEvent({
                 'div_id': this.divid,
                 'code': this.editor.getValue(),
+                'lang': this.language,
                 'errinfo': 'success',
                 'to_save': saveCode,
                 'prefix': this.pretext,
@@ -839,6 +850,7 @@ ActiveCode.prototype.runProg = function () {
                 self.logRunEvent({
                     'div_id': self.divid,
                     'code': self.editor.getValue(),
+                    'lang': this.langauge,
                     'errinfo': err.toString(),
                     'to_save': saveCode,
                     'prefix': self.pretext,
@@ -1014,9 +1026,18 @@ String.prototype.replaceAll = function (target, replacement) {
 };
 
 AudioTour.prototype = new RunestoneBase();
-
 // function to display the audio tours
 function AudioTour (divid, code, bnum, audio_text) {
+    this.isClicked = false;
+    this.audio_tour = null;
+    this.audio_code = null;
+    this.windowcode = null;
+    this.first_audio = null;
+    this.prev_audio = null;
+    this.pause_audio = null;
+    this.next_audio = null;
+    this.last_audio = null;
+    this.status = null;
     this.elem = null; // current audio element playing
     this.currIndex = null; // current index
     this.len = null; // current length of audio files for tour
@@ -1027,7 +1048,6 @@ function AudioTour (divid, code, bnum, audio_text) {
     this.afile = null; // file name for audio
     this.playing = false; // flag to say if playing or not
     this.tourName = "";
-
     // Replacing has been done here to make sure special characters in the code are displayed correctly
     code = code.replaceAll("*doubleq*", "\"");
     code = code.replaceAll("*singleq*", "'");
@@ -1035,7 +1055,6 @@ function AudioTour (divid, code, bnum, audio_text) {
     code = code.replaceAll("*close*", ")");
     code = code.replaceAll("*nline*", "<br/>");
     var codeArray = code.split("\n");
-
     var audio_hash = [];
     var bval = [];
     var atype = audio_text.replaceAll("*doubleq*", "\"");
@@ -1045,7 +1064,6 @@ function AudioTour (divid, code, bnum, audio_text) {
         var aword = audio_type[i].split(";");
         bval.push(aword[0]);
     }
-
     var first = "<pre><div id='" + divid + "_l1'>" + "1.   " + codeArray[0] + "</div>";
     var num_lines = codeArray.length;
     for (var i = 1; i < num_lines; i++) {
@@ -1064,103 +1082,117 @@ function AudioTour (divid, code, bnum, audio_text) {
     //laying out the HTML content
 
     var bcount = 0;
-    var html_string = "<div class='modal-lightsout'></div><div class='modal-profile'><h3>Take an audio tour!</h3><div class='modal-close-profile'></div><p id='windowcode'></p><p id='" + divid + "_audiocode'></p>";
-    html_string += "<p id='status'></p>";
-    html_string += "<input type='image' src='../_static/first.png' width='25' id='first_audio' name='first_audio' title='Play first audio in tour' alt='Play first audio in tour' onerror=\"this.onerror=null;this.src='_static/first.png'\" disabled/>" +
-                   "<input type='image' src='../_static/prev.png' width='25' id='prev_audio' name='prev_audio' title='Play previous audio in tour' alt='Play previous audio in tour' onerror=\"this.onerror=null;this.src='_static/prev.png'\" disabled/>" +
-                   "<input type='image' src='../_static/pause.png' width='25' id='pause_audio' name='pause_audio' title='Pause current audio' alt='Pause current audio' onerror=\"this.onerror=null;this.src='_static/pause.png'\" disabled/>" + "" +
-                   "<input type='image' src='../_static/next.png' width ='25' id='next_audio' name='next_audio' title='Play next audio in tour' alt='Play next audio in tour' onerror=\"this.onerror=null;this.src='_static/next.png'\" disabled/>" +
-                   "<input type='image' src='../_static/last.png' width ='25' id='last_audio' name='last_audio' title='Play last audio in tour' alt='Play last audio in tour' onerror=\"this.onerror=null;this.src='_static/last.png'\" disabled/><br/>";
-    for (var i = 0; i < audio_type.length - 1; i++) {
-        html_string += "<input type='button' style='margin-right:5px;' class='btn btn-default btn-sm' id='button_audio_" + i + "' name='button_audio_" + i + "' value=" + bval[i] + " />";
-        bcount++;
-    }
-    //html_string += "<p id='hightest'></p><p id='hightest1'></p><br/><br/><p id='test'></p><br/><p id='audi'></p></div>";
-    html_string += "</div>";
+    
+    this.audio_tour = document.createElement("div");
+    this.audio_tour.align = "center";
 
-    var tourdiv = document.createElement('div');
-    document.body.appendChild(tourdiv);
-    $(tourdiv).html(html_string);
-    $('#windowcode').html(first);
+    this.audio_code = document.createElement("p");
 
-    // Position modal box
-    $.fn.center = function () {
-        this.css("position", "absolute");
-        // y position
-        this.css("top", ($(window).scrollTop() + $(navbar).height() + 10 + "px"));
-        // show window on the left so that you can see the output from the code still
-        this.css("left", ($(window).scrollLeft() + "px"));
-        return this;
-    };
+    this.windowcode = document.createElement("div");
+    this.windowcode.align = "left";
+    $(this.windowcode).html(first);
 
-    $(".modal-profile").center();
-    $('.modal-profile').fadeIn("slow");
-    //$('.modal-lightsout').css("height", $(document).height());
-    $('.modal-lightsout').fadeTo("slow", .5);
-    $('.modal-close-profile').show();
+    this.first_audio = document.createElement("button");
+    this.prev_audio = document.createElement("button");
+    this.pause_audio = document.createElement("button");
+    this.next_audio = document.createElement("button");
+    this.last_audio = document.createElement("button");
 
-    // closes modal box once close link is clicked, or if the lights out divis clicked
-    $('.modal-close-profile, .modal-lightsout').click( (function () {
+    this.first_audio.className = "glyphicon glyphicon-fast-backward";
+    this.prev_audio.className = "glyphicon glyphicon-step-backward";
+    this.pause_audio.className = "glyphicon glyphicon-play";
+    this.next_audio.className = "glyphicon glyphicon-step-forward";
+    this.last_audio.className = "glyphicon glyphicon-fast-forward";
+
+    this.first_audio.style = "height: 25px; width: 25px;"
+    this.prev_audio.style = "height: 25px; width: 25px;"
+    this.pause_audio.style = "height: 25px; width: 25px;"
+    this.next_audio.style = "height: 25px; width: 25px;"
+    this.last_audio.style = "height: 25px; width: 25px;"
+
+    this.first_audio.name = "first_audio";
+    this.prev_audio.name = "prev_audio";
+    this.pause_audio.name = "pause_audio";
+    this.next_audio.name = "next_audio";
+    this.last_audio.name = "last_audio";
+
+    this.first_audio.title = "Play first audio in tour";
+    this.prev_audio.title = "Play previous audio in tour";
+    this.pause_audio.title = "Play audio";
+    this.next_audio.title = "Play next audio in tour";
+    this.last_audio.title = "Play last audio in tour";
+
+    this.first_audio.setAttribute("aria-label", "Play first audio in tour");
+    this.prev_audio.setAttribute("aria-label", "Play previous audio in tour");
+    this.pause_audio.setAttribute("aria-label", "Play audio");
+    this.next_audio.setAttribute("aria-label", "Play next audio in tour");
+    this.last_audio.setAttribute("aria-label", "Play last audio in tour");
+
+    this.first_audio.disabled = true;
+    this.prev_audio.disabled = true;
+    this.pause_audio.disabled = true;
+    this.next_audio.disabled = true;
+    this.last_audio.disabled = true;
+
+    this.status = document.createElement("div");
+    this.status.className = "alert alert-info";
+    this.status.style= "display: inline-block; margin-top: 7px;";
+
+    $(this.audio_tour).append(this.audio_code, this.windowcode, document.createElement("br"), this.first_audio, this.prev_audio, this.pause_audio, this.next_audio, this.last_audio, document.createElement("br"), this.status);
+    $("#"+divid+" .ac_code_div").append(this.audio_tour);
+    $('#'+divid+' .ac_opt.btn.btn-default:last-child').show();
+    $('#'+divid+' .CodeMirror.cm-s-default.ui-resizable').hide();
+    $('#'+divid+' .ac_opt.btn.btn-default:nth-last-child(2)').hide();
+
+    $('#'+divid+' .ac_opt.btn.btn-default:last-child').click( (function () {
         if (this.playing) {
             this.elem.pause();
         }
         //log change to db
         this.logBookEvent({'event': 'Audio', 'act': 'closeWindow', 'div_id': divid});
-        $('.modal-profile').fadeOut("slow");
-        $('.modal-lightsout').fadeOut("slow");
-        document.body.removeChild(tourdiv);
+        $(this.audio_tour).remove();
+        $('#'+divid+' .ac_opt.btn.btn-default:last-child').hide();
+        $('#'+divid+' .CodeMirror.cm-s-default.ui-resizable').show();
+        $('#'+divid+' .ac_opt.btn.btn-default:nth-last-child(2)').show();
     }).bind(this));
-
-    // Accommodate buttons for a maximum of five tours
-
-    $('#' + 'button_audio_0').click((function () {
-        this.tour(divid, audio_hash[0], bcount);
-    }).bind(this));
-    $('#' + 'button_audio_1').click((function () {
-        this.tour(divid, audio_hash[1], bcount);
-    }).bind(this));
-    $('#' + 'button_audio_2').click((function () {
-        this.tour(divid, audio_hash[2], bcount);
-    }).bind(this));
-    $('#' + 'button_audio_3').click((function () {
-        this.tour(divid, audio_hash[3], bcount);
-    }).bind(this));
-    $('#' + 'button_audio_4').click((function () {
-        this.tour(divid, audio_hash[4], bcount);
-    }).bind(this));
+    this.tour(divid, audio_hash[0], bcount);
 
     // handle the click to go to the next audio
-    $('#first_audio').click((function () {
+    $(this.first_audio).click((function () {
         this.firstAudio();
     }).bind(this));
 
     // handle the click to go to the next audio
-    $('#prev_audio').click((function () {
+    $(this.prev_audio).click((function () {
         this.prevAudio();
     }).bind(this));
-
     // handle the click to pause or play the audio
-    $('#pause_audio').click((function () {
-        this.pauseAndPlayAudio();
+    $(this.pause_audio).click((function () {
+        if (!this.isClicked) {
+            this.pause_audio.className = "glyphicon glyphicon-pause";
+            this.pause_audio.title = "Pause current audio";
+            this.pause_audio.setAttribute("aria-label", "Pause audio");
+            // start at the first audio
+            if (this.currIndex == null) {
+                this.currIndex = 0;
+            }
+            // play the first audio in the tour
+            this.playCurrIndexAudio();
+        } else {
+            this.pauseAndPlayAudio(divid);
+        }
+        this.isClicked = true;
     }).bind(this));
 
     // handle the click to go to the next audio
-    $('#next_audio').click((function () {
+    $(this.next_audio).click((function () {
         this.nextAudio();
     }).bind(this));
 
     // handle the click to go to the next audio
-    $('#last_audio').click((function () {
+    $(this.last_audio).click((function () {
         this.lastAudio();
     }).bind(this));
-
-    // make the image buttons look disabled
-    $("#first_audio").css('opacity', 0.25);
-    $("#prev_audio").css('opacity', 0.25);
-    $("#pause_audio").css('opacity', 0.25);
-    $("#next_audio").css('opacity', 0.25);
-    $("#last_audio").css('opacity', 0.25);
-
 }
 
 AudioTour.prototype.tour = function (divid, audio_type, bcount) {
@@ -1169,16 +1201,17 @@ AudioTour.prototype.tour = function (divid, audio_type, bcount) {
     this.theDivid = divid;
 
     // enable prev, pause/play and next buttons and make visible
-    $('#first_audio').removeAttr('disabled');
-    $('#prev_audio').removeAttr('disabled');
-    $('#pause_audio').removeAttr('disabled');
-    $('#next_audio').removeAttr('disabled');
-    $('#last_audio').removeAttr('disabled');
-    $("#first_audio").css('opacity', 1.0);
-    $("#prev_audio").css('opacity', 1.0);
-    $("#pause_audio").css('opacity', 1.0);
-    $("#next_audio").css('opacity', 1.0);
-    $("#last_audio").css('opacity', 1.0);
+    $(this.first_audio).removeAttr('disabled');
+    $(this.prev_audio).removeAttr('disabled');
+    $(this.pause_audio).removeAttr('disabled');
+    $(this.next_audio).removeAttr('disabled');
+    $(this.last_audio).removeAttr('disabled');
+
+    $(this.first_audio).css('opacity', 1.0);
+    $(this.prev_audio).css('opacity', 1.0);
+    $(this.pause_audio).css('opacity', 1.0);
+    $(this.next_audio).css('opacity', 1.0);
+    $(this.last_audio).css('opacity', 1.0);
 
     // disable tour buttons
     for (var i = 0; i < bcount; i++)
@@ -1187,7 +1220,7 @@ AudioTour.prototype.tour = function (divid, audio_type, bcount) {
     var atype = audio_type.split(";");
     var name = atype[0].replaceAll("\"", " ");
     this.tourName = name;
-    $('#status').html("Starting the " + name);
+    $(this.status).html("Starting the " + name);
 
     //log tour type to db
     this.logBookEvent({'event': 'Audio', 'act': name, 'div_id': divid});
@@ -1221,31 +1254,17 @@ AudioTour.prototype.tour = function (divid, audio_type, bcount) {
         this.ahash[akey] = lnums;
         this.aname.push(akey);
     }
-    var ahtml = "#" + divid + "_audiocode";
-    $(ahtml).html(str); // set the html to the audio tags
+    $(this.audio_code).html(str);
     this.len = this.aname.length; // set the number of audio file in the tour
-
-    // start at the first audio
-    this.currIndex = 0;
-
-    // play the first audio in the tour
-    this.playCurrIndexAudio();
 };
 
 AudioTour.prototype.handlePlaying = function() {
-
     // if this.playing audio pause it
-    if (this.playing) {
-
-        this.elem.pause();
-
-        // unbind current ended
-        $('#' + this.afile).unbind('ended');
-
-        // unhighlight the prev lines
-        this.unhighlightLines(this.theDivid, this.ahash[this.aname[this.currIndex]]);
-    }
-
+    this.elem.pause();
+    // unbind current ended
+    $('#' + this.afile).unbind('ended');
+    // unhighlight the prev lines
+    this.unhighlightLines(this.theDivid, this.ahash[this.aname[this.currIndex]]);
 };
 
 AudioTour.prototype.firstAudio = function () {
@@ -1334,24 +1353,12 @@ AudioTour.prototype.playCurrIndexAudio = function () {
 
 // handle the end of the tour
 AudioTour.prototype.handleTourEnd = function () {
-
-    $('#status').html(" The " + this.tourName + " Ended");
-
-    // disable the prev, pause/play, and next buttons and make them more invisible
-    $('#first_audio').attr('disabled', 'disabled');
-    $('#prev_audio').attr('disabled', 'disabled');
-    $('#pause_audio').attr('disabled', 'disabled');
-    $('#next_audio').attr('disabled', 'disabled');
-    $('#last_audio').attr('disabled', 'disabled');
-    $("#first_audio").css('opacity', 0.25);
-    $("#prev_audio").css('opacity', 0.25);
-    $("#pause_audio").css('opacity', 0.25);
-    $("#next_audio").css('opacity', 0.25);
-    $("#last_audio").css('opacity', 0.25);
-
-    // enable the tour buttons
-    for (var j = 0; j < this.buttonCount; j++)
-        $('#button_audio_' + j).removeAttr('disabled');
+    $(this.status).html("The Audio Tour has ended. You may restart the tour by clicking 'Play'");
+    this.pause_audio.className = "glyphicon glyphicon-play";
+    this.pause_audio.title = "Play audio";
+    this.pause_audio.setAttribute("aria-label", "Play audio");
+    this.isClicked = false;
+    this.currIndex = null;
 };
 
 // only call this one after the first time
@@ -1386,17 +1393,17 @@ AudioTour.prototype.outerAudio = function () {
 AudioTour.prototype.playWhenReady = function (afile, divid, ahash) {
     // unbind current
     $('#' + afile).unbind('canplaythrough');
-    //console.log("in playWhenReady " + elem.duration);
-
-    $('#status').html("Playing the " + this.tourName);
     this.elem.currentTime = 0;
-    this.highlightLines(divid, ahash[afile]);
-    $('#' + afile).bind('ended', (function () {
-        this.outerAudio();
-    }).bind(this));
     this.playing = true;
-    this.elem.play();
-
+    //console.log("in playWhenReady " + elem.duration);
+    this.highlightLines(divid, ahash[afile]);
+    if (this.pause_audio.className === "glyphicon glyphicon-pause") {
+        $(this.status).html("Playing the " + this.tourName);
+        $('#' + afile).bind('ended', (function () {
+        this.outerAudio();
+        }).bind(this));
+        this.elem.play();
+    }
 };
 
 
@@ -1409,7 +1416,7 @@ AudioTour.prototype.playaudio = function (i, aname, divid, ahash) {
     //console.log("in playaudio " + elem.duration);
     if (isNaN(this.elem.duration) || this.elem.duration == 0) {
         // set the status
-        $('#status').html("Loading audio.  Please wait.   If it doesn't start soon close this window (click on the red X) and try again");
+        $(this.status).html("Loading audio.  Please wait.   If the tour doesn't start soon click on 'Stop Tour' and try again.");
         $('#' + this.afile).bind('canplaythrough', (function () {
             this.playWhenReady(this.afile, divid, ahash);
         }).bind(this));
@@ -1421,16 +1428,17 @@ AudioTour.prototype.playaudio = function (i, aname, divid, ahash) {
 };
 
 // pause if this.playing and play if paused
-AudioTour.prototype.pauseAndPlayAudio = function () {
-    var btn = document.getElementById('pause_audio');
+AudioTour.prototype.pauseAndPlayAudio = function (divid) {
+    var btn = this.pause_audio;
 
     // if paused and clicked then continue from current
     if (this.elem.paused) {
         // calcualte the time left to play in milliseconds
         counter = (this.elem.duration - this.elem.currentTime) * 1000;
         this.elem.play(); // start the audio from current spot
-        document.getElementById("pause_audio").src = "../_static/pause.png";
-        document.getElementById("pause_audio").title = "Pause current audio";
+        this.pause_audio.className = "glyphicon glyphicon-pause";
+        this.pause_audio.title = "Pause current audio";
+        this.pause_audio.setAttribute("aria-label", "Pause audio");
         //log change to db
         this.logBookEvent({'event': 'Audio', 'act': 'play', 'div_id': this.theDivid});
     }
@@ -1438,8 +1446,9 @@ AudioTour.prototype.pauseAndPlayAudio = function () {
     // if audio was this.playing pause it
     else if (this.playing) {
         this.elem.pause(); // pause the audio
-        document.getElementById("pause_audio").src = "../_static/play.png";
-        document.getElementById("pause_audio").title = "Play paused audio";
+        this.pause_audio.className = "glyphicon glyphicon-play";
+        this.pause_audio.title = "Play paused audio";
+        this.pause_audio.setAttribute("aria-label", "Play paused audio");
         //log change to db
         this.logBookEvent({'event': 'Audio', 'act': 'pause', 'div_id': this.theDivid});
     }
