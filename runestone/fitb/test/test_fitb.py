99a25af56de2f7f7590910507aed2cb99da36b45
Bryan A. Jones <bjones@ece.msstate.edu>
2017-08-03 17:00:04 -0500

    Add: Tests to verify error handling in fitb directive.

--- runestone/fitb/test/test_fitb.py
+++ runestone/fitb/test/test_fitb.py
@@ -1,16 +1,41 @@
+from unittest import TestCase
 from runestone.unittest_base import module_fixture_maker, RunestoneTestCase
 
-setUpModule, tearDownModule = module_fixture_maker(__file__)
+mf, setUpModule, tearDownModule = module_fixture_maker(__file__, True)
+# Look for errors producted by invalid questions.
+class FITB_Error_Tests(TestCase):
+    def test_1(self):
+        # Check for the following directive-level errors.
+        directive_level_errors =  (
+            # Produced my mchoice id error1_no_content,
+            (20, 'Content block expected for the "fillintheblank" directive; none found.'),
+            (32, 'Not enough feedback for the number of blanks supplied.'),
+        )
+        for error_line, error_string in directive_level_errors:
+            # The rst_prolog in conf.py confuses line numbers. Adjust for it.
+            self.assertIn(':{}: WARNING: {}'.format(error_line + 4, error_string), mf.build_stderr_data)
+
+        # Check for the following error inside the directive.
+        inside_directive_errors = (
+            # error2,
+            (24, 'the last item in a fill-in-the-blank question must be a bulleted list.'),
+            # error3,
+            (30, 'each list item in a fill-in-the-blank problems must contain only one item, a field list.'),
+        )
+        for error_line, error_string in inside_directive_errors:
+            # The rst_prolog in conf.py confuses line numbers. Adjust for it.
+            self.assertIn(': WARNING: On line {}, {}'.format(error_line + 4, error_string), mf.build_stderr_data)
+
+        # Make sure we saw all errors.
+        self.assertEqual(len(directive_level_errors) + len(inside_directive_errors), mf.build_stderr_data.count('WARNING'))
+
 
 class FITBtests(RunestoneTestCase):
     ## Helpers
     ## =======
-    def setUp(self):
-        super(FITBtests, self).setUp()
-        self.driver.get(self.host + "/index.html") # Access page
-
-    # Return the DIV containing a FITB question.
+    # Load the web page, then return the DIV containing a FITB question.
     def find_fitb(self):
+        self.driver.get(self.host + "/index.html")
         self.fitb = self.driver.find_element_by_id("fill1412")
         return self.fitb
 
@@ -37,15 +62,15 @@ class FITBtests(RunestoneTestCase):
         self.find_blank(0).send_keys("red")
         self.click_checkme()
         feedback = self.find_feedback()
-        self.assertIsNotNone(feedback.text)
+        self.assertIn('Correct', feedback.text)
 
-    # No answers yet -- Incorrect feedback
+    # No answers yet -- no answer provided feedback.
     def test_fitb2(self):
         self.find_fitb()
         self.click_checkme()
         feedback = self.find_feedback()
         self.assertIsNotNone(feedback.text)
-        self.assertIn("Incorrect",feedback.text)
+        self.assertIn("No answer provided.", feedback.text)
 
 
     # Both correct answers
