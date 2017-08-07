91e5620525949e5cad91db1550487b9f3bb78062
Brad Miller <bonelake@mac.com>
2017-08-07 15:44:05 -0500

    Much simpler way of walking the TOC

--- runestone/chapterdb/dbchapterinfo.py
+++ runestone/chapterdb/dbchapterinfo.py
@@ -19,9 +19,11 @@ __author__ = 'bmiller'
 
 import re, datetime
 import os.path
+from collections import OrderedDict
 import docutils
 from sqlalchemy import Table, select, and_
 from runestone.server.componentdb import engine, meta
+ignored_chapters = ["", "FrontBackMatter", "Appendices"]
 
 def setup(app):
     """
@@ -29,57 +31,19 @@ def setup(app):
     we are not going for a new directive or role we can use the extension mechanism to add
     specific event handlers.
     """
-    app.connect('doctree-resolved', doctree_resolved)
-    app.connect('build-finished', build_finished)
-
-chap_order = [] # correct chapter order from the index.rst
-sub_ids_for_chapter = {} # list of subchapter ids fromthe doctree for each chapter
-chaptitles = {} # chapter titles from the doctree
-subtitles = {} # The proper title of each sub chapter from the doctree
-subchap_order = {}  # order of the subchapters, gleaned from the toctree.rst file
-
-def doctree_resolved(app, doctree, docname):
-    """
-    Called in response to the doctree-resolved even in Sphinx.  This means that for
-    a given document the doctree is complete and the TOC is fully realized.
-
-    * Walk the sections in this document, grabbing the nicely formatted title, in fact we only
-      want the first title as we don't track sub-sub sections.
-    * If this is a toctree document then read that document to get the correct order.abs
-    * If this is the top level index document then do the same for the toctree inclusions
-
-    """
-    for section in doctree.traverse(docutils.nodes.section):
-        #print(section.source ,dir(section.document))
-        title = section.next_node(docutils.nodes.Titular)
-        # Section.source is something like ``/abs/path/to/chap/subchap.rst``. Set ``chap_id = 'chap'`` and ``subchap_id = 'subchap'``.
-        chap_id = os.path.basename(os.path.dirname(section.source))
-        subchap_id = os.path.splitext(os.path.basename(section.source))[0]
-        if subchap_id == 'index' and chap_order == []:
-            chap_order.extend(get_top_toc(section.source))
-        if chap_id not in sub_ids_for_chapter:
-            sub_ids_for_chapter[chap_id] = []
-        if chap_id not in subtitles:
-            subtitles[chap_id] = {}
-        if title:
-            if subchap_id == 'toctree':
-                subchap_order[chap_id] = get_toctree(section.source)
-                chaptitles[chap_id] = title.astext()
-            if subchap_id not in sub_ids_for_chapter[chap_id]:
-                sub_ids_for_chapter[chap_id].append(subchap_id)
-            if subchap_id not in subtitles[chap_id]:
-                subtitles[chap_id][subchap_id] = title.astext()
+    app.connect('env-updated', env_updated)
 
 
-def build_finished(app, ex):
+def update_database(chaptitles, subtitles, app):
     """
     When the build is completely finished output the information gathered about
     chapters and subchapters into the database.
     """
     if not engine:
         print("You need to install a DBAPI module - psycopg2 for Postgres")
+        print("Or perhaps you have not set your DBURL environment variable")
         return
-    print("Populating the database with Chapter information")
+
     course_id = app.env.config.html_context.get('course_id', "unknown")
     chapters = Table('chapters', meta, autoload=True, autoload_with=engine)
     sub_chapters = Table('sub_chapters', meta, autoload=True, autoload_with=engine)
@@ -87,17 +51,16 @@ def build_finished(app, ex):
     basecourse = app.config.html_context.get('basecourse',"unknown")
     print("Cleaning up old chapters info")
     engine.execute(chapters.delete().where(chapters.c.course_id == course_id))
-    if 'Labs' in sub_ids_for_chapter:
-        chap_order.append('Labs')
-        subchap_order['Labs'] = sub_ids_for_chapter['Labs']
-    for chap in chap_order:
+
+    print("Populating the database with Chapter information")
+    for chap in chaptitles:
         # insert row for chapter in the chapter table and get the id
         print(u"Adding chapter subchapter info for {}".format(chap))
         ins = chapters.insert().values(chapter_name=chaptitles.get(chap, chap),
                                        course_id=course_id, chapter_label=chap)
         res = engine.execute(ins)
         currentRowId = res.inserted_primary_key[0]
-        for sub in subchap_order[chap]:
+        for sub in subtitles[chap]:
             # insert row for subchapter
             q_name = u"{}/{}".format(chaptitles.get(chap,chap), subtitles[chap][sub])
             ins = sub_chapters.insert().values(sub_chapter_name=subtitles[chap][sub],
@@ -121,37 +84,46 @@ def build_finished(app, ex):
                                             timestamp=datetime.datetime.now(),   
                                             base_course=basecourse)
                 engine.execute(ins)
-    
 
-def get_toctree(path):
+
+def env_updated(app, env):
     """
-    Process the toctree for a chapter.
+    This may be the best place to walk the completed document with TOC
     """
-    all_lines = open(path, 'r').readlines()
-    ix = -1
-    found = False
-    # find the toctree directive
-    for ix, line in enumerate(all_lines):
-        if ".. toctree::" in line:
-            found = True
-            break
+    print("ENV-UPDATED")
+    relations = env.collect_relations()
+    included_docs = []
+    updated_docs = []
+    # Each relation is a list in the following order [parent, prev_doc, next_doc]
+    cur_doc = env.config.master_doc
+    while cur_doc:
+        included_docs.append(cur_doc)
+        doctree = env.get_doctree(cur_doc)
+        cur_doc = relations[cur_doc][2]
 
-    if ix == -1 or not found:
-        raise TypeError("This file does not appear to contain a toctree directive")
+    chap_titles = OrderedDict()
+    subchap_titles = OrderedDict()
 
-    # now read parameter line until we find a blank.
-    iy = 0
-    for iy, line in enumerate(all_lines[ix+1:]):
-        if re.match(r'^\s*$', line):
-            break
+    for docname in included_docs:
+        doctree = env.get_doctree(docname)
+        for section in doctree.traverse(docutils.nodes.section):
+            updated_docs.append(docname)
+            title = section.next_node(docutils.nodes.Titular)
+            print(docname, title)
+            chap_id = os.path.dirname(docname)
+            subchap_id = os.path.basename(docname)
+            if chap_id in ignored_chapters or subchap_id == "index" :
+                continue
+            if chap_id not in chap_titles:
+                if subchap_id == 'toctree':
+                    chap_titles[chap_id] = title.astext()
+                else:
+                    chap_titles[chap_id] = chap_id
+            if chap_id not in subchap_titles:
+                subchap_titles[chap_id] = OrderedDict()
+            if subchap_id not in subchap_titles[chap_id] and subchap_id != 'toctree':
+                subchap_titles[chap_id][subchap_id] = title.astext()
 
-    return [x.strip().replace('.rst', '') for x in all_lines[ix+iy+2:] if not re.match(r'^\s*$', x)]
+    update_database(chap_titles, subchap_titles, app)
 
-
-def get_top_toc(path):
-    """
-    Process the top level toctree for the index.rst file.
-    """
-    all_lines = open(path, 'r').readlines()
-
-    return [x.strip().replace('/toctree.rst', '') for x in all_lines if "toctree.rst" in x]
+    return []
