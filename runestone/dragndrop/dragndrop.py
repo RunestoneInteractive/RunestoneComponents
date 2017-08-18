46817ede3ae705c2c87f72de435ae857d9f7ce6b
Brad Miller <bonelake@mac.com>
2017-08-18 16:54:22 -0500

    elipses in docstring interpreted as a comment by sphinx when building a preview

--- runestone/dragndrop/dragndrop.py
+++ runestone/dragndrop/dragndrop.py
@@ -103,7 +103,7 @@ class DragNDrop(RunestoneDirective):
     :match_1: Draggable element text|||Dropzone to be matched with text
     :match_2: Drag to Answer B|||Answer B
     :match_3: Draggable text|||Text of dropzone
-    ...etc...(up to 20 matches)
+    etc. (up to 20 matches)
 
     The question goes here.
     """
