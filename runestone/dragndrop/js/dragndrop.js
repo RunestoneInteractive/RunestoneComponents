RunestoneInteractive/RunestoneComponents

commit 0561b8372365e9b6e5d1c32b9f0fc407eed8ae96

Set button types to button

runestone/dragndrop/js/dragndrop.js
@@ -150,6 +150,7 @@ DragNDrop.prototype.createButtons = function () {
     $(this.submitButton).attr({
         "class": "btn btn-success drag-button",
         "name": "do answer",
+        "type": "button",
     });
 
     this.submitButton.onclick = function () {
@@ -338,9 +339,11 @@ DragNDrop.prototype.dragEval = function (logFlag) {
     this.correctNum = this.dragNum - this.incorrectNum - this.unansweredNum;
     this.setLocalStorage({"correct": (this.correct ? "T" : "F")});
     this.renderFeedback();
-    if (logFlag)   // Sometimes we don't want to log the answers--for example, on re-load of a timed exam
-        var answer = this.pregnantIndexArray.join(";");
+    if (logFlag) {  // Sometimes we don't want to log the answers--for example, on re-load of a timed exam
+        let answer = this.pregnantIndexArray.join(";");
+        answer = answer + ":" + this.correctNum + "/" + this.dragNum ;
         this.logBookEvent({"event": "dragNdrop", "act": answer, "answer":answer, "minHeight": this.minheight, "div_id": this.divid, "correct": this.correct});
+    }
 };
 
 DragNDrop.prototype.renderFeedback = function () {

runestone/clickableArea/js/clickable.js
@@ -112,7 +112,8 @@ ClickableArea.prototype.createButtons = function () {
     this.submitButton.textContent = "Check Me";
     $(this.submitButton).attr({
         "class": "btn btn-success",
-        "name": "do answer"
+        "name": "do answer",
+        "type": "button",
     });
 
     this.submitButton.onclick = function () {

runestone/fitb/js/fitb.js
@@ -84,7 +84,8 @@ FITB.prototype.renderFITBButtons = function () {
     this.submitButton.textContent = "Check Me";
     $(this.submitButton).attr({
         "class": "btn btn-success",
-        "name": "do answer"
+        "name": "do answer",
+        "type": "button",
     });
     this.submitButton.addEventListener("click", function () {
         this.startEvaluation(true);

runestone/parsons/js/parsons.js
@@ -1196,6 +1196,7 @@ Parsons.prototype.initializeView = function () {
 	this.checkButton.textContent = "Check Me";
 	this.checkButton.id = this.counterId + "-check";
 	this.parsonsControlDiv.appendChild(this.checkButton);
+	this.checkButton.type = "button";
 	this.checkButton.addEventListener('click', function(event) {
 		event.preventDefault();
 		that.checkMe();
@@ -1204,6 +1205,7 @@ Parsons.prototype.initializeView = function () {
 	$(this.resetButton).attr("class", "btn btn-default");
 	this.resetButton.textContent = "Reset";
 	this.resetButton.id = this.counterId + "-reset";
+	this.resetButton.type = "button";
 	this.parsonsControlDiv.appendChild(this.resetButton);
 	this.resetButton.addEventListener('click', function(event) {
 		event.preventDefault();

commit 0ff8d3aebe057851cf3585b3119e67311a9df511

Do not include the "correct/total" results of dragndrop in logged answers
This was mistakenly added in https://github.com/RunestoneInteractive/RunestoneComponents/commit/0561b8372365e9b6e5d1c32b9f0fc407eed8ae96, which broke the dragndrop directive causing an exception when loading saved answers and preventing dragndrop answers from being autograded.

Potential fix for https://github.com/RunestoneInteractive/RunestoneServer/issues/1025

runestone/dragndrop/js/dragndrop.js
@@ -341,7 +341,6 @@ DragNDrop.prototype.dragEval = function (logFlag) {
     this.renderFeedback();
     if (logFlag) {  // Sometimes we don't want to log the answers--for example, on re-load of a timed exam
         let answer = this.pregnantIndexArray.join(";");
-        answer = answer + ":" + this.correctNum + "/" + this.dragNum ;
         this.logBookEvent({"event": "dragNdrop", "act": answer, "answer":answer, "minHeight": this.minheight, "div_id": this.divid, "correct": this.correct});
     }
 };