c5e459e37d64b9d5bd2803bfde817a12d03a69fa
DaveParillo <dave.parillo@gmail.com>
2017-10-21 16:38:33 -0700

    added fitb unit tests for casei option and numeric ranges

--- runestone/fitb/test/test_fitb.py
+++ runestone/fitb/test/test_fitb.py
@@ -7,9 +7,9 @@ class FITB_Error_Tests(TestCase):
     def test_1(self):
         # Check for the following directive-level errors.
         directive_level_errors =  (
-            # Produced my mchoice id error1_no_content,
-            (20, 'Content block expected for the "fillintheblank" directive; none found.'),
-            (32, 'Not enough feedback for the number of blanks supplied.'),
+            # Produced my fitb id error1_no_content,
+            (34, 'Content block expected for the "fillintheblank" directive; none found.'),
+            (46, 'Not enough feedback for the number of blanks supplied.'),
         )
         for error_line, error_string in directive_level_errors:
             # The rst_prolog in conf.py confuses line numbers. Adjust for it.
@@ -18,9 +18,9 @@ class FITB_Error_Tests(TestCase):
         # Check for the following error inside the directive.
         inside_directive_errors = (
             # error2,
-            (24, 'the last item in a fill-in-the-blank question must be a bulleted list.'),
+            (38, 'the last item in a fill-in-the-blank question must be a bulleted list.'),
             # error3,
-            (30, 'each list item in a fill-in-the-blank problems must contain only one item, a field list.'),
+            (44, 'each list item in a fill-in-the-blank problems must contain only one item, a field list.'),
         )
         for error_line, error_string in inside_directive_errors:
             # The rst_prolog in conf.py confuses line numbers. Adjust for it.
@@ -34,9 +34,9 @@ class FITBtests(RunestoneTestCase):
     ## Helpers
     ## =======
     # Load the web page, then return the DIV containing a FITB question.
-    def find_fitb(self):
+    def find_fitb(self, elem_id):
         self.driver.get(self.host + "/index.html")
-        self.fitb = self.driver.find_element_by_id("fill1412")
+        self.fitb = self.driver.find_element_by_id(elem_id)
         return self.fitb
 
     # Find one of the blanks, based on the given index.
@@ -48,8 +48,8 @@ class FITBtests(RunestoneTestCase):
         self.fitb.find_element_by_tag_name('button').click()
 
     # Find the question's feedback element.
-    def find_feedback(self):
-        return self.fitb.find_element_by_id("fill1412_feedback")
+    def find_feedback(self, elem_id):
+        return self.fitb.find_element_by_id(elem_id + "_feedback")
 
     ## Tests
     ## =====
@@ -58,32 +58,32 @@ class FITBtests(RunestoneTestCase):
         '''
         http://runestoneinteractive.org/build/html/directives.html#fill-in-the-blank for documentation
         '''
-        self.find_fitb()
+        self.find_fitb("fill1412")
         self.find_blank(0).send_keys("red")
         self.click_checkme()
-        feedback = self.find_feedback()
+        feedback = self.find_feedback("fill1412")
         self.assertIn('Correct', feedback.text)
 
     # No answers yet -- no answer provided feedback.
     def test_fitb2(self):
-        self.find_fitb()
+        self.find_fitb("fill1412")
         self.click_checkme()
-        feedback = self.find_feedback()
+        feedback = self.find_feedback("fill1412")
         self.assertIsNotNone(feedback.text)
         self.assertIn("No answer provided.", feedback.text)
 
 
     # Both correct answers
     def test_fitb3(self):
-        self.find_fitb()
+        self.find_fitb("fill1412")
         self.find_blank(0).send_keys("red")
         self.find_blank(1).send_keys("away")
         self.click_checkme()
-        feedback = self.find_feedback()
+        feedback = self.find_feedback("fill1412")
         self.assertIn("Correct", feedback.text)
 
     def test_fitb4(self):
-        self.find_fitb()
+        self.find_fitb("fill1412")
         blank0 = self.find_blank(0)
         # Type an incorrect answer.
         blank0.send_keys("red")
@@ -93,5 +93,36 @@ class FITBtests(RunestoneTestCase):
         blank0.send_keys("red")
         self.find_blank(1).send_keys("away")
         self.click_checkme()
-        feedback = self.find_feedback()
+        feedback = self.find_feedback("fill1412")
         self.assertIn("Correct", feedback.text)
+
+    def test_fitboneblank_too_low(self):
+        self.find_fitb("fill_2pi")
+        self.find_blank(0).send_keys(" 6")
+        self.click_checkme()
+        feedback = self.find_feedback("fill_2pi")
+        self.assertIn('Try higher.', feedback.text)
+
+    def test_fitboneblank_wildcard(self):
+        self.find_fitb("fill_2pi")
+        self.find_blank(0).send_keys("I give up")
+        self.click_checkme()
+        feedback = self.find_feedback("fill_2pi")
+        self.assertIn('Incorrect. Try again.', feedback.text)
+
+    def test_fitbfillrange(self):
+        self.find_fitb("fill_2pi")
+        self.find_blank(0).send_keys(" 6.28 ")
+        self.click_checkme()
+        feedback = self.find_feedback("fill_2pi")
+        self.assertIn('Good job.', feedback.text)
+
+    def test_fitbregex(self):
+        self.find_fitb("fillregex")
+        self.find_blank(0).send_keys(" mARy ")
+        self.find_blank(1).send_keys("LITTLE")
+        self.find_blank(2).send_keys("2")
+        self.click_checkme()
+        feedback = self.find_feedback("fillregex")
+        self.assertIn('Correct.', feedback.text)
+
