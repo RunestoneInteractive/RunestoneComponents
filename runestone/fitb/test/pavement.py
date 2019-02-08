802f4f928e4c3009d9a16eb82ccd3fb104dc3585
Bryan A. Jones <bjones@ece.msstate.edu>
2019-02-08 08:42:11 -0600

    Update: Re-create fitb test from template.

--- runestone/fitb/test/pavement.py
+++ runestone/fitb/test/pavement.py
@@ -6,36 +6,50 @@ import os, sys
 from runestone.server import get_dburl
 from sphinxcontrib import paverutils
 import pkg_resources
+from socket import gethostname
 
 sys.path.append(os.getcwd())
 
-home_dir = os.getcwd()
-master_url = 'http://127.0.0.1:8000'
-master_app = 'runestone'
-serving_dir = "./build/testfitb"
+# The project name, for use below.
+project_name = "testfitb"
+# The root directory for ``runestone serve``.
+serving_dir = "./build/" + project_name
+# The destination directory for ``runestone deploy``.
 dest = "../../static"
 
 options(
     sphinx = Bunch(docroot=".",),
 
     build = Bunch(
-        builddir="./build/testfitb",
+        builddir=serving_dir,
         sourcedir="_sources",
-        outdir="./build/testfitb",
+        outdir=serving_dir,
         confdir=".",
-        project_name = "testfitb",
-        template_args={'course_id': 'testfitb',
+        template_args={'course_id': project_name,
                        'login_required':'false',
-                       'appname':master_app,
+                       'appname': 'runestone',
                        'loglevel': 0,
-                       'course_url':master_url,
+                       'course_url': 'http://127.0.0.1:8000',
                        'use_services': 'false',
                        'python3': 'false',
                        'dburl': '',
-                       'basecourse': 'testfitb'
+                       'default_ac_lang': 'python',
+                       'basecourse': 'testfitb',
+                       'jobe_server': 'http://jobe2.cosc.canterbury.ac.nz',
+                       'proxy_uri_runs': '/jobe/index.php/restapi/runs/',
+                       'proxy_uri_files': '/jobe/index.php/restapi/files/',
+                       'downloads_enabled': 'false',
+                       'enable_chatcodes': 'false',
+                       'allow_pairs': 'false'
                         }
     )
 )
+
+# if we are on runestone-deploy then use the proxy server not canterbury
+if gethostname() == 'runestone-deploy':
+    del options.build.template_args['jobe_server']
+    del options.build.template_args['proxy_uri_runs']
+    del options.build.template_args['proxy_uri_files']
 
 version = pkg_resources.require("runestone")[0].version
 options.build.template_args['runestone_version'] = version
