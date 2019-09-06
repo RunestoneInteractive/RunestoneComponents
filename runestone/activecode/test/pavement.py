228375ec0ebf58a6fc408c42c0145cd292c59b38
Brad Miller <brad@runestoneinteractive.com>
2019-09-06 12:44:03 +0000

    correct use_services value

--- runestone/activecode/test/pavement.py
+++ runestone/activecode/test/pavement.py
@@ -29,7 +29,7 @@ options(
                        'appname':master_app,
                        'loglevel': 0,
                        'course_url':master_url,
-                       'use_services': False,
+                       'use_services': 'false',
                        'python3': 'true',
                        'dburl': '', 'downloads_enabled': 'true', 'enable_chatcodes': 'false', 'allow_pairs': 'false',
                        'basecourse': 'activecodetest',
