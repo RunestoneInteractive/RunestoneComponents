7e9f5abc24d9ac9e0be8e46927cd58486712d36d
Brad Miller <brad@runestoneinteractive.com>
2019-09-21 11:56:00 +0000

    New Release
    * more consistent question labels
    * partner works in all languages
    * add instructor code sharing
    * more robust unit tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as fh:
     dependencies = [l.strip() for l in fh]
 
-VERSION='4.1.4'
+VERSION='4.1.5'
 setup(
     name='runestone',
     description='Sphinx extensions for writing interactive documents.',
