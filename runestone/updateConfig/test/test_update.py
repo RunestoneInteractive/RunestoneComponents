7ab3f185f6f1ec1265abf3603a48881eecda038d
Jovan Maric <33927662+maric993@users.noreply.github.com>
2019-10-24 17:10:04 +0200

    new updateconfig directive and tests

---
+++ runestone/updateConfig/test/test_update.py
@@ -0,0 +1,70 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+import time
+from runestone.unittest_base import module_fixture_maker, RunestoneTestCase
+
+setUpModule, tearDownModule = module_fixture_maker(__file__)
+class UpdateConfigTests(RunestoneTestCase):
+    def find_fitb(self, elem_id):
+        self.driver.get(self.host + "/index.html")
+        wait = WebDriverWait(self.driver, 10)
+        try:
+            wait.until(
+                EC.presence_of_element_located((By.ID, elem_id))
+            )
+        except:
+            text = self.driver.page_source
+            print(text[:300])
+
+        self.fitb = self.driver.find_element_by_id(elem_id)
+        return self.fitb
+
+    def test_updateconfig_fitb(self):
+        self.driver.get(self.host + "/index.html")
+        
+        self.find_fitb("fill1")
+        button_before_config_update = self.driver.find_element_by_id("question1_bcomp")
+
+        self.find_fitb("fill2")
+        button_after_config_update = None
+        try:
+            button_after_config_update = self.driver.find_element_by_id("question2_bcomp")
+        except NoSuchElementException:
+            pass
+        finally:
+            self.assertIsNone(button_after_config_update)
+    
+        self.find_fitb("fill3")
+        button_after_second_config_update = self.driver.find_element_by_id("question3_bcomp")
+
+        
+        self.assertIsNotNone(button_before_config_update)
+        self.assertIsNotNone(button_after_second_config_update)
+
+    def test_updateconfig_multiplechoice(self):
+        self.driver.get(self.host + "/index.html")
+        
+        self.find_fitb("question1")
+        button_before_config_update = self.driver.find_element_by_id("fill1_bcomp")
+
+        self.find_fitb("question2")
+        button_after_config_update = None
+        try:
+            button_after_config_update = self.driver.find_element_by_id("fill2_bcomp")
+        except NoSuchElementException:
+            pass
+        finally:
+            self.assertIsNone(button_after_config_update)
+    
+        self.find_fitb("question3")
+        button_after_second_config_update = self.driver.find_element_by_id("fill3_bcomp")
+
+        
+        self.assertIsNotNone(button_before_config_update)
+        self.assertIsNotNone(button_after_second_config_update)
+
+
+ 
---
+++ runestone/updateConfig/update.py
@@ -0,0 +1,36 @@
+__author__ = 'petlja'
+from ast import literal_eval
+from docutils import nodes
+from docutils.parsers.rst import Directive
+from docutils.parsers.rst import directives
+from runestone.common.runestonedirective import RunestoneDirective , RunestoneIdDirective , RunestoneNode
+
+
+def setup(app):
+    app.add_directive('update-config', UpdateConfig)
+
+class UpdateConfig(RunestoneDirective):
+    """
+    update config
+    """ 
+    required_arguments = 0
+    optional_arguments = 1
+    has_content = False
+    option_spec = RunestoneDirective.option_spec.copy()
+    option_spec.update({'set_config_option': directives.unchanged,
+    })
+
+        
+    def run(self):
+        env = self.state.document.settings.env
+
+        if 'set_config_option' in self.options:
+            config_opt , value = self.options['set_config_option'].split(" ", 1)
+
+            if(len(value)>1000):
+                self.error("Length of configuration option value exceeded")
+
+            env.config[config_opt]= literal_eval(value)
+
+        
+        return []
