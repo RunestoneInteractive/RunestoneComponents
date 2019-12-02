ba46365e3cb43c4203f7d654432984fe5eb221e8
natstall <stallsmithn@berea.edu>
2019-12-02 13:25:18 -0500

    Created dark theme.

---
+++ runestone/common/js/theme.js
@@ -0,0 +1,25 @@
+function getSwitch(){
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }
+}
+
+function switchTheme() {
+
+	var checkBox = document.getElementById("checkbox");
+    if (checkBox.checked == true) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
