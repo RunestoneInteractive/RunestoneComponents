9622ce005414519a720e7cc057b2b215b3b2e1b8
Brad Miller <brad@runestoneinteractive.com>
2020-03-28 15:01:29 +0000

    New Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as fh:
     dependencies = [l.strip() for l in fh]
 
-VERSION = "4.2.3"
+VERSION = "4.2.4"
 setup(
     name="runestone",
     description="Sphinx extensions for writing interactive documents.",
