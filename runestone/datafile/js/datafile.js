cf6dbb003745419e47e3f9275a85f3120440b798
Brad Miller <brad@runestoneinteractive.com>
2020-04-13 13:22:22 +0000

    add datafile to component_factory

--- runestone/datafile/js/datafile.js
+++ runestone/datafile/js/datafile.js
@@ -73,3 +73,11 @@ $(document).ready(function () {
         }
     });
 });
+
+if (typeof window.component_factory === "undefined") {
+    window.component_factory = {};
+}
+
+window.component_factory.datafile = function (opts) {
+    return new DataFile(opts);
+};
