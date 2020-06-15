dfd433956a3d2a17d62c57ae7cf2ba2e872cb0da
Brad Miller <brad@runestoneinteractive.com>
2020-06-15 21:57:39 +0000

    Fix warnings and add python_requires >= 3.6
    also contains experimental code to do pre-install and post-install
    hooks for running scripts.  This would be very useful for times when
    we need to require some non-python stuff.
    We may even be able to use this to check for npm and then post development
    install (-e) run npm run build.

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from subprocess import check_call
+import sys
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     dependencies = [l.strip() for l in fh]
 
-VERSION = "5.0.4"
+VERSION = "5.0.5"
+
+# These pre-install hooks are useful to make sure any pre-requisite
+# programs that are not pip installable are in place.
+# class PreInstallCommand(install):
+#     def run(self):
+#         print("Running pre-install commands")
+#         check_call(["flummox"])
+#         install.run(self)
+
+
+# class PreDevelopCommand(develop):
+#     def run(self):
+#         print("Running pre-develop commands")
+#         try:
+#             check_call(["flummox"])
+#         except:
+#             print("You will need to install flummox before proceeding")
+#             sys.exit(-1)
+#         develop.run(self)
+
+
 setup(
     name="runestone",
     description="Sphinx extensions for writing interactive documents.",
@@ -12,6 +37,7 @@ setup(
     author_email="bonelake@mac.com",
     packages=find_packages(exclude=["*.*.test"]),
     install_requires=dependencies,
+    python_requires=">=3.6",
     include_package_data=True,
     zip_safe=False,
     package_dir={"runestone": "runestone"},
@@ -21,8 +47,10 @@ setup(
     download_url="https://github.com/RunestoneInteractive/RunestoneComponents/tarball/{}".format(
         VERSION
     ),
+    # uncomment below to activate any pre-install hook checks
+    # cmdclass={"install": PreInstallCommand, "develop": PreDevelopCommand},
     keywords=["runestone", "sphinx", "ebook", "oer", "education"],  # arbitrary keywords
-    classifiers=(
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Environment :: Plugins",
@@ -33,11 +61,12 @@ setup(
         "Operating System :: MacOS",
         "Operating System :: Unix",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Education",
         "Topic :: Text Processing :: Markup",
-    ),
+    ],
     # data_files=[('common',['runestone/common/*']),
     #             ('project/template', ['newproject_copy_me/*'])
     # ],
