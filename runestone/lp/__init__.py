f024907e963b3e9b625fb21db4b4130213a77b3c
Bryan A. Jones <bjones@ece.msstate.edu>
2020-07-10 22:03:14 -0500

    Add: For loop directive.

--- runestone/lp/__init__.py
+++ runestone/lp/__init__.py
@@ -3,6 +3,9 @@
 # ********************************
 from runestone.lp import inlinesyntaxhighlight, lp
 
+# Register the for loop directive with Sphinx.
+from runestone.lp import for_loop  # NOQA: F401
+
 
 # Initialize both extensions.
 def setup(app):
