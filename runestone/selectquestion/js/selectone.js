a77a041170c408734b1ae81b6edcda961af09d80
Brad Miller <brad@runestoneinteractive.com>
2020-07-16 12:35:09 +0000

    Do it right: handle async nature of selectquestion

--- runestone/selectquestion/js/selectone.js
+++ runestone/selectquestion/js/selectone.js
@@ -7,31 +7,47 @@ import RunestoneBase from "../../common/js/runestonebase";
 export default class SelectOne extends RunestoneBase {
     constructor(opts) {
         super(opts);
+        this.origOpts = opts;
         this.questions = $(opts.orig).data("questionlist");
-        let newid = `q_${Math.floor(Math.random() * 1000000)}`;
-        opts.orig.id = newid;
-        let data = { questions: this.questions };
+        this.newid = `q_${Math.floor(Math.random() * 1000000)}`;
+        opts.orig.id = this.newid;
+    }
+
+    initialize() {
         let self = this;
-        $.getJSON("/runestone/ajax/get_question_source", data, function (
-            htmlsrc
-        ) {
-            let res;
-            if (opts.timed) {
-                res = createTimedComponent(htmlsrc, { timed: true });
-                // replace the entry in the timed exam's list of components
-                // with the component created by createTimedComponent
-                for (let component of opts.rqa) {
-                    if (component.question == self) {
-                        component.question = res;
-                        break;
+        let data = { questions: this.questions };
+        let opts = this.origOpts;
+        let newid = this.newid;
+        let myPromise = new Promise(
+            function (resolve, reject) {
+                $.getJSON(
+                    "/runestone/ajax/get_question_source",
+                    data,
+                    function (htmlsrc) {
+                        let res;
+                        if (opts.timed) {
+                            res = createTimedComponent(htmlsrc, {
+                                timed: true,
+                            });
+                            // replace the entry in the timed exam's list of components
+                            // with the component created by createTimedComponent
+                            for (let component of opts.rqa) {
+                                if (component.question == self) {
+                                    component.question = res;
+                                    break;
+                                }
+                            }
+                            self.realComponent = res;
+                            self.containerDiv = res.containerDiv;
+                        } else {
+                            res = renderRunestoneComponent(htmlsrc, newid, {});
+                        }
+                        resolve("done");
                     }
-                }
-                self.realComponent = res;
-                self.containerDiv = res.containerDiv;
-            } else {
-                res = renderRunestoneComponent(htmlsrc, newid, {});
-            }
-        });
+                );
+            }.bind(this)
+        );
+        return myPromise;
     }
 }
 
@@ -42,7 +58,8 @@ $(document).bind("runestone:login-complete", function () {
                 $(this).closest("[data-component=timedAssessment]").length == 0
             ) {
                 // If this element exists within a timed component, don't render it here
-                new SelectOne({ orig: this });
+                let tmp = new SelectOne({ orig: this });
+                tmp.initialize();
             }
         } catch (err) {
             console.log(`Error rendering New Exercise ${this.id}
