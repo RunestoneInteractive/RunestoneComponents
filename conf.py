9efdcfa667b4b943827123bb5aabc7d348bec84b
bjones1 <bjones@ece.msstate.edu>
2020-12-03 13:23:34 -0600

    Docs: Link to hsblog endpoint in server.

--- conf.py
+++ conf.py
@@ -347,9 +347,10 @@ htmlhelp_basename = "PythonCoursewareProjectdoc"
 # This config value contains the locations and names of other projects that should be linked to in this documentation.
 intersphinx_mapping = {
     "RunestoneServer": (
-        "https://runestoneserver.readthedocs.io/en/latest/",
-        None,
-        # An optional local file location -- useful when updating both Sphinx builds before the updates are pushed to the server.
-        ##"F:/Runestone/web2py/applications/runestone/_build/objects.inv",
+        "https://runestoneserver.readthedocs.io/en/latest/", (
+            None,
+            # An optional local file location -- useful when updating both Sphinx builds before the updates are pushed to the server.
+            ##"C:/Users/bjones/Documents/git/web2py/applications/runestone/_build/objects.inv",
+        )
     )
 }
