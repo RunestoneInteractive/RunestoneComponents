6993e64fb033deffd24a211bd98b13a75b500ad4
bjones1 <bjones@ece.msstate.edu>
2021-03-17 22:49:56 +0100

    Fix: Rewrite poll to use new testing framework.

--- runestone/poll/test/test_poll.py
+++ runestone/poll/test/test_poll.py
@@ -1,36 +1,25 @@
-from runestone.unittest_base import module_fixture_maker, RunestoneTestCase
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
+def _test_poll(selenium_utils, id):
+    """ test the poll directive """
+    self = selenium_utils
+    self.wait_until_ready(id)
+
+    poll_div = self.driver.find_element_by_id(id)
+
+    opts = poll_div.find_elements_by_css_selector("input[type='radio']")
+
+    # the poll in overview should be on a scale 1-10.
+    assert len(opts) == 10, "Not enough poll options present!"
+
+    # just choose option 4
+    poll_div.find_element_by_id(f"{id}_opt_4").click()
+
+    el = poll_div.find_element_by_id(f"{id}_sent")  # check for results span
+    assert el.text[:6] == "Thanks"
+
+    # just make sure we can find the results div - an exception will be raised if the div cannot be found
+    poll_div.find_element_by_id(f"{id}_results")
 
 
-setUpModule, tearDownModule = module_fixture_maker(__file__)
-
-
-class PollTests(RunestoneTestCase):
-    def test_poll(self):
-        """ test the poll directive """
-        self.driver.get(self.host + "/index.html")
-        wait = WebDriverWait(self.driver, 10)
-        try:
-            wait.until(EC.presence_of_element_located((By.ID, "pollid1_container")))
-        except:
-            text = self.driver.page_source
-            print(text[:300])
-
-        poll_div = self.driver.find_element_by_id("pollid1_container")
-
-        opts = poll_div.find_elements_by_css_selector("input[type='radio']")
-
-        # the poll in overview should be on a scale 1-10.
-        self.assertTrue(len(opts) == 10, "Not enough poll options present!")
-
-        # just choose option 4
-        poll_div.find_element_by_id("pollid1_opt_4").click()
-
-        el = poll_div.find_element_by_id("pollid1_sent")  # check for results span
-        self.assertIsNotNone(el)
-        self.assertEqual(el.text[:6], "Thanks")
-
-        # just make sure we can find the results div - an exception will be raised if the div cannot be found
-        poll_div.find_element_by_id("pollid1_results")
+def test_poll(selenium_utils):
+    selenium_utils.get("index.html")
+    _test_poll(selenium_utils, "pollid1")
