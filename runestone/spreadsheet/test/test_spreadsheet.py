fcfa7ee6b4e3ec0baa09cccb8b97b61655b342ce
bjones1 <bjones@ece.msstate.edu>
2021-03-18 21:58:33 +0100

    Fix: Enable the video and spreadsheet components to indicate when they're ready.

--- runestone/spreadsheet/test/test_spreadsheet.py
+++ runestone/spreadsheet/test/test_spreadsheet.py
@@ -1,38 +1,19 @@
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-import time
-from runestone.unittest_base import module_fixture_maker, RunestoneTestCase
+def _test_ss_autograde(selenium_utils):
+    selenium_utils.wait_until_ready("test_spreadsheet_1")
 
-setUpModule, tearDownModule = module_fixture_maker(__file__)
+    t2 = selenium_utils.driver.find_element_by_id("ss1")
+    rb = t2.find_element_by_class_name("run-button")
+    rb.click()
+    out = selenium_utils.driver.find_element_by_id("ss1_stdout")
+    assert "You passed 2 out of 2 tests" in out.text
+
+    selenium_utils.wait_until_ready("test_spreadsheet_2")
+    t2 = selenium_utils.driver.find_element_by_id("ss2")
+    rb = t2.find_element_by_class_name("run-button")
+    rb.click()
+    out = selenium_utils.driver.find_element_by_id("ss2_stdout")
+    assert "You passed 1 out of 1 tests" in out.text
 
 
-class SpreadsheetTests(RunestoneTestCase):
-    def test_ss_autograde(self):
-        self.driver.get(self.host + "/index.html")
-        wait = WebDriverWait(self.driver, 10)
-        try:
-            wait.until(EC.presence_of_element_located((By.ID, "ss1")))
-        except:
-            text = self.driver.page_source
-            print(text[:300])
-
-        t2 = self.driver.find_element_by_id("ss1")
-        self.assertIsNotNone(t2)
-        time.sleep(1)
-        rb = t2.find_element_by_class_name("run-button")
-        self.assertIsNotNone(rb)
-        rb.click()
-        out = self.driver.find_element_by_id("ss1_stdout")
-        self.assertTrue("You passed 2 out of 2 tests" in out.text)
-
-        t2 = self.driver.find_element_by_id("ss2")
-        self.assertIsNotNone(t2)
-        time.sleep(1)
-        rb = t2.find_element_by_class_name("run-button")
-        self.assertIsNotNone(rb)
-        rb.click()
-        out = self.driver.find_element_by_id("ss2_stdout")
-        print(out.text)
-        self.assertTrue("You passed 1 out of 1 tests" in out.text)
+def test_ss_autograde(selenium_utils):
+    selenium_utils.get("index.html")
