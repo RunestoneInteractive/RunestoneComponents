1950f185cccaf185eeffe2455a7d7e25258bc817
bjones1 <bjones@ece.msstate.edu>
2021-03-28 00:48:35 +0100

    Add: smart waits Port: to pytest.

--- runestone/reveal/test/test_reveal.py
+++ runestone/reveal/test/test_reveal.py
@@ -4,61 +4,54 @@ Test Reveal Answer question directive
 
 __author__ = "yasinovskyy"
 
-from runestone.unittest_base import module_fixture_maker, RunestoneTestCase
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
+def test_r1(selenium_utils):
+    """Initial view. Content is hidden"""
+    su = selenium_utils
+    su.get("index.html")
+    su.wait_until_ready("question1")
 
-setUpModule, tearDownModule = module_fixture_maker(__file__)
+    q1 = su.driver.find_element_by_css_selector("#question1 div")
+    cnamestr = q1.get_attribute("style")
+    assert "display: none;" == cnamestr
 
 
-class RevealQuestion_Tests(RunestoneTestCase):
-    def test_r1(self):
-        """Initial view. Content is hidden"""
-        self.driver.get(self.host + "/index.html")
-        wait = WebDriverWait(self.driver, 10)
-        try:
-            wait.until(EC.presence_of_element_located((By.ID, "reveal")))
-        except:
-            text = self.driver.page_source
-            print(text[:300])
+def test_r2(selenium_utils):
+    """Reveal button clicked"""
+    su = selenium_utils
+    su.get("index.html")
+    su.wait_until_ready("question1")
+    t1 = su.driver.find_element_by_id("reveal")
 
-        t1 = self.driver.find_element_by_id("reveal")
+    btn_show = t1.find_element_by_id("question1_show")
+    btn_show.click()
 
-        q1 = t1.find_element_by_id("question1")
+    q1 = t1.find_element_by_id("question1")
 
-        cnamestr = q1.get_attribute("style")
-        self.assertEqual("display: none;", cnamestr)
+    cnamestr = q1.get_attribute("style")
+    assert "display: none;" != cnamestr
 
-    def test_r2(self):
-        """Reveal button clicked"""
-        self.driver.get(self.host + "/index.html")
-        t1 = self.driver.find_element_by_id("reveal")
 
-        btn_show = t1.find_element_by_id("question1_show")
-        btn_show.click()
+def test_r3(selenium_utils):
+    """Content is revealed, then hidden again"""
+    su = selenium_utils
+    su.get("index.html")
+    su.wait_until_ready("question1")
+    t1 = su.driver.find_element_by_id("reveal")
 
-        q1 = t1.find_element_by_id("question1")
+    btn_show = t1.find_element_by_id("question1_show")
+    btn_show.click()
+    btn_hide = t1.find_element_by_id("question1_hide")
+    btn_hide.click()
 
-        cnamestr = q1.get_attribute("style")
-        self.assertNotEqual("display: none;", cnamestr)
+    q1 = su.driver.find_element_by_css_selector("#question1 div")
+    cnamestr = q1.get_attribute("style")
+    assert "display: none;" == cnamestr
 
-    def test_r3(self):
-        """Content is revealed, then hidden again"""
-        self.driver.get(self.host + "/index.html")
-        t1 = self.driver.find_element_by_id("reveal")
 
-        btn_show = t1.find_element_by_id("question1_show")
-        btn_show.click()
-        btn_hide = t1.find_element_by_id("question1_hide")
-        btn_hide.click()
-
-        q1 = t1.find_element_by_id("question1")
-
-        cnamestr = q1.get_attribute("style")
-        self.assertEqual("display: none;", cnamestr)
-
-    def test_r4(self):
-        """Check for is_instructor test """
-        t1 = self.driver.find_element_by_id("reveal")
-        self.assertIn("{{ if is_instructor: }}", t1.get_attribute("innerHTML"))
+def test_r4(selenium_utils):
+    """Check for is_instructor test """
+    su = selenium_utils
+    su.get("index.html")
+    su.wait_until_ready("question1")
+    t1 = su.driver.find_element_by_id("reveal")
+    assert "{{ if is_instructor: }}" in t1.get_attribute("innerHTML")
