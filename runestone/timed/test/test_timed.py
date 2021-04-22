0b915841e992c89471c91a69f27485aacd91d78e
bjones1 <bjones@ece.msstate.edu>
2021-04-22 20:16:34 +0000

    Fix: Update timed tests to be more testable. Add: A lot more tests of components using the timed test feature.

--- runestone/timed/test/test_timed.py
+++ runestone/timed/test/test_timed.py
@@ -1,108 +1,139 @@
+from pathlib import Path
 import time
 
-from runestone.unittest_base import module_fixture_maker, RunestoneTestCase
+import pytest
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-setUpModule, tearDownModule = module_fixture_maker(__file__)
+
+def start(selenium_utils, timed_divid):
+    selenium_utils.wait_until_ready(timed_divid)
+    start_element = selenium_utils.driver.find_element_by_id("start")
+    start_element.click()
 
 
-class TimedTests(RunestoneTestCase):
-    def start(self, timed_divid):
-        self.driver.execute_script("window.localStorage.clear();")
-        ##self.wait_until_ready(timed_divid)
-        time.sleep(1)
-        start = self.driver.find_element_by_id("start")
-        start.click()
+def finish(selenium_utils):
+    selenium_utils.scroll_to_top()
+    finish = selenium_utils.wait.until(
+        EC.element_to_be_clickable((By.ID, "finish"))
+    )
+    finish.click()
 
-    def finish(self):
-        finish = self.wait.until(
-            EC.element_to_be_clickable((By.ID, "finish"))
-        )
-        self.assertIsNotNone(finish)
-        finish.click()
+    alert = selenium_utils.driver.switch_to.alert
+    alert.accept()
 
-        alert = self.driver.switch_to_alert()
-        alert.accept()
 
-    def dragndrop(self, src, dest):
-        ActionChains(self.driver).drag_and_drop(src, dest).perform()
+def dragndrop(selenium_utils, src, dest):
+    ActionChains(selenium_utils.driver).drag_and_drop(src, dest).perform()
 
-    def js_dragndrop(self):
-        with open("../../dragndrop/test/drag_and_drop_helper.js") as f:
-            self.driver.execute_script(f.read())
 
-    def test_one_question_timed_exam(self):
-        self.driver.get(self.host + "/index.html")
-        self.start("time_q1")
+def js_dragndrop(selenium_utils):
+    with open(Path(__file__).parent / "../../dragndrop/test/drag_and_drop_helper.js") as f:
+        selenium_utils.driver.execute_script(f.read())
 
-        t1 = self.driver.find_element_by_id("time_test_1_q1_form")
-        t1.find_element_by_id("time_test_1_q1_opt_0").click()
-        t1.find_element_by_id("time_test_1_q1_opt_1").click()
-        t1.find_element_by_id("time_test_1_q1_opt_2").click()
-        t1.find_element_by_id("time_test_1_q1_opt_3").click()
 
-        self.finish()
+def get_divid(selenium_utils, div_id):
+    selenium_utils.wait_until_ready(div_id)
+    return selenium_utils.driver.find_element_by_id(div_id)
 
-        fb = t1.find_element_by_id("time_test_1_q1_eachFeedback_1")
-        self.assertIsNotNone(fb)
-        cnamestr = fb.get_attribute("class")
-        self.assertEqual(cnamestr, "eachFeedback alert alert-danger")
 
-    def test_1(self):
-        self.driver.get(self.host + "/multiquestion.html")
-        self.start("timed1")
-        next = self.driver.find_element_by_id("next")
+def test_one_question_timed_exam(selenium_utils_get):
+    start(selenium_utils_get, "time_q1")
 
-        # Select answer A in the mchoice question (which is correct).
-        self.driver.find_element_by_id("questiontimed1_1_opt_0").click()
-        next.click()
+    t1 = selenium_utils_get.driver.find_element_by_id("time_test_1_q1_form")
+    t1.find_element_by_id("time_test_1_q1_opt_0").click()
+    t1.find_element_by_id("time_test_1_q1_opt_1").click()
+    t1.find_element_by_id("time_test_1_q1_opt_2").click()
+    t1.find_element_by_id("time_test_1_q1_opt_3").click()
 
-        # Click the correct cells in the table. There should be only one table.
-        time.sleep(1)
-        next.click()
+    finish(selenium_utils_get)
 
-        # The drag and drop question.
-        time.sleep(1)
-        next.click()
+    fb = t1.find_element_by_id("time_test_1_q1_eachFeedback_1")
+    cnamestr = fb.get_attribute("class")
+    assert cnamestr == "eachFeedback alert alert-danger"
 
-        # Fill in the blank question
-        ##self.wait_until_ready("fill1412")
-        time.sleep(1)
-        filb = self.driver.find_element_by_id("fill1412")
-        blank1, blank2 = filb.find_elements_by_tag_name("input")
-        blank1.send_keys("red")
-        blank2.send_keys("away")
-        next.click()
 
-        # ActiveCode question
-        ##self.wait_until_ready("units2")
-        time.sleep(1)
-        code_mirror = self.driver.find_element_by_class_name("CodeMirror")
-        code_mirror.find_elements_by_class_name("CodeMirror-line")[1].click()
-        code_mirror.find_element_by_css_selector("textarea").send_keys(" - 4 + a + b")
-        next.click()
+@pytest.fixture
+def selenium_utils_mq(selenium_utils):
+    selenium_utils.get("multiquestion.html")
+    return selenium_utils
 
-        # The Parson's problem.
-        ##self.wait_until_ready("morning")
-        time.sleep(1)
-        source = self.driver.find_element_by_id("parsons-1-source")
-        answer = self.driver.find_element_by_id("parsons-1-answer")
-        self.dragndrop(source.find_element_by_id("parsons-1-block-0"), answer)
-        time.sleep(1)
-        self.dragndrop(source.find_element_by_id("parsons-1-block-2"), answer)
-        time.sleep(1)
-        self.dragndrop(source.find_element_by_id("parsons-1-block-1"), answer)
-        next.click()
 
-        # The short answer question.
-        ##self.wait_until_ready("question2")
-        time.sleep(1)
-        self.driver.find_element_by_id("question2_solution").send_keys("ROYGBIV circle area")
+# Split the test this way so that server-side tests can use a different div_id for the selectquestion timed problem.
+def test_1(selenium_utils_mq):
+    _test_1(selenium_utils_mq, "test_timed_1")
 
-        self.finish()
-        results = self.driver.find_element_by_id("timed1results")
-        assert "Num Correct: 4" in results.text
-        assert "Num Wrong: 0" in results.text
-        assert "Num Skipped: 3" in results.text
+
+def _test_1(selenium_utils_mq, timed_divid):
+    start(selenium_utils_mq, timed_divid)
+    next = selenium_utils_mq.driver.find_element_by_id("next")
+
+    # Select answer A in the mchoice question (which is correct).
+    div_id = "test_timed_mchoice_1"
+    selenium_utils_mq.wait_until_ready(div_id)
+    selenium_utils_mq.driver.find_element_by_id(f"{div_id}_opt_0").click()
+    next.click()
+
+    # Click the correct cells in the table. There should be only one table.
+    table = get_divid(selenium_utils_mq, "test_timed_clickablearea_1")
+    first_ellipsis = True
+    for elem in table.find_elements_by_css_selector("p"):
+        if elem.text == "correct":
+            elem.click()
+        if "…" in elem.text:
+            if first_ellipsis:
+                first_ellipsis = False
+            else:
+                elem.click()
+    next.click()
+
+    # The drag and drop question.
+    div_id = "test_timed_dnd_1"
+    dnd = get_divid(selenium_utils_mq, div_id)
+    js_dragndrop(selenium_utils_mq)
+    src, dest = dnd.find_elements_by_class_name("rsdraggable")
+    src_items = src.find_elements_by_tag_name("span")
+    for i in range(3):
+        selenium_utils_mq.driver.execute_script(f"""$("#{src_items[i].get_attribute('id')}").simulateDragDrop({{ dropTarget: 'span:contains("Answer {src_items[i].text[8:]}")' }})""")
+    selenium_utils_mq.scroll_to_top()
+    next.click()
+
+    # Fill in the blank question
+    filb = get_divid(selenium_utils_mq, "test_timed_fitb_1")
+    blank1, blank2 = filb.find_elements_by_tag_name("input")
+    blank1.send_keys("red")
+    blank2.send_keys("away")
+    next.click()
+
+    # ActiveCode question
+    div_id = "test_timed_activecode_1"
+    selenium_utils_mq.wait_until_ready(div_id)
+    selenium_utils_mq.driver.execute_script(
+        f"""window.edList['{div_id}'].editor.setValue("def add(a, b): return a + b")"""
+    )
+    next.click()
+
+    # The Parson's problem.
+    selenium_utils_mq.wait_until_ready("test_timed_parsons_1")
+    # This is an ugly hack, since Parson's problems number problems sequentially instead of based on the problem's ID. The div_id happens to match the ordering of Parson's problems...
+    parsons_counter = timed_divid[-1]
+    source = selenium_utils_mq.driver.find_element_by_id(f"parsons-{parsons_counter}-source")
+    answer = selenium_utils_mq.driver.find_element_by_id(f"parsons-{parsons_counter}-answer")
+    dragndrop(selenium_utils_mq, source.find_element_by_id(f"parsons-{parsons_counter}-block-0"), answer)
+    time.sleep(1)
+    dragndrop(selenium_utils_mq, source.find_element_by_id(f"parsons-{parsons_counter}-block-2"), answer)
+    time.sleep(1)
+    dragndrop(selenium_utils_mq, source.find_element_by_id(f"parsons-{parsons_counter}-block-1"), answer)
+    next.click()
+
+    # The short answer question.
+    div_id = "test_timed_shortanswer_1"
+    selenium_utils_mq.wait_until_ready(div_id)
+    selenium_utils_mq.driver.find_element_by_id(f"{div_id}_solution").send_keys("ROYGBIV circle area")
+
+    finish(selenium_utils_mq)
+    results = selenium_utils_mq.driver.find_element_by_id(f"{timed_divid}results")
+    assert "Num Correct: 6" in results.text
+    assert "Num Wrong: 0" in results.text
+    assert "Num Skipped: 1" in results.text
