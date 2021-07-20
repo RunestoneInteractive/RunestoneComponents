889645d2af0ecf67698ad98ef62cb4d38df01942
bjones1 <bjones@ece.msstate.edu>
2021-07-20 11:33:32 +0100

    Fix: Run Black, update template.

--- runestone/wavedrom/test/pavement.py
+++ runestone/wavedrom/test/pavement.py
@@ -7,16 +7,18 @@ from paver.easy import options, Bunch
 import paver.setuputils
 
 from runestone import get_master_url
-from runestone import build  # NOQA: F401 -- build is called implicitly by the paver driver.
+from runestone import (
+    build,
+)  # NOQA: F401 -- build is called implicitly by the paver driver.
 from runestone.server import get_dburl
 
 paver.setuputils.install_distutils_tasks()
 sys.path.append(os.getcwd())
 
 # The project name, for use below.
-project_name = 'wavedrom'
+project_name = "wavedrom"
 
-master_url = 'http://127.0.0.1:8000'
+master_url = "http://127.0.0.1:8000"
 if not master_url:
     master_url = get_master_url()
 
@@ -26,40 +28,43 @@ serving_dir = "./build/" + project_name
 dest = "../../static"
 
 options(
-    sphinx=Bunch(docroot=".",),
-
+    sphinx=Bunch(
+        docroot=".",
+    ),
     build=Bunch(
         builddir=serving_dir,
         sourcedir="_sources",
         outdir=serving_dir,
         confdir=".",
         template_args={
-            'course_title': project_name,
-            'python3': 'false',
-            'dburl': '',
-            'default_ac_lang': 'python',
-            'downloads_enabled': 'false',
-            'enable_chatcodes': 'false',
-            'allow_pairs': 'false',
-            'dynamic_pages': False,
-            'use_services': 'false',
-            'basecourse': project_name,
-            'course_id': project_name,
+            "login_required": "false",
+            "loglevel": 0,
+            "course_title": project_name,
+            "python3": "false",
+            "dburl": "",
+            "default_ac_lang": "python",
+            "downloads_enabled": "false",
+            "enable_chatcodes": "false",
+            "allow_pairs": "false",
+            "dynamic_pages": False,
+            "use_services": "false",
+            "basecourse": project_name,
+            "course_id": project_name,
             # These are used for non-dynamic books.
-            'appname': 'runestone',
-            'course_url': master_url,
-        }
-    )
+            "appname": "runestone",
+            "course_url": master_url,
+        },
+    ),
 )
 
 # if we are on runestone-deploy then use the proxy server not canterbury
-if gethostname() == 'runestone-deploy':
-    del options.build.template_args['jobe_server']
-    del options.build.template_args['proxy_uri_runs']
-    del options.build.template_args['proxy_uri_files']
+if gethostname() == "runestone-deploy":
+    del options.build.template_args["jobe_server"]
+    del options.build.template_args["proxy_uri_runs"]
+    del options.build.template_args["proxy_uri_files"]
 
 version = pkg_resources.require("runestone")[0].version
-options.build.template_args['runestone_version'] = version
+options.build.template_args["runestone_version"] = version
 
 # If DBURL is in the environment override dburl
-options.build.template_args['dburl'] = get_dburl(outer=locals())
+options.build.template_args["dburl"] = get_dburl(outer=locals())
--- runestone/wavedrom/wavedrom.py
+++ runestone/wavedrom/wavedrom.py
@@ -38,34 +38,35 @@ class WaveDromDirective(SphinxDirective):
     """
     Directive to insert arbitrary dot markup.
     """
+
     has_content = True
     required_arguments = 0
     optional_arguments = 0
     final_argument_whitespace = False
     option_spec = {
-        'alt': directives.unchanged,
-        'align': align_spec,
-        'caption': directives.unchanged,
-        'name': directives.unchanged,
-        'class': directives.class_option,
+        "alt": directives.unchanged,
+        "align": align_spec,
+        "caption": directives.unchanged,
+        "name": directives.unchanged,
+        "class": directives.class_option,
     }
 
     def run(self) -> List[Node]:
         node = wavedrom()
-        node['code'] = '\n'.join(self.content)
-        node['options'] = {'docname': self.env.docname}
-        if 'alt' in self.options:
-            node['alt'] = self.options['alt']
-        if 'align' in self.options:
-            node['align'] = self.options['align']
-        if 'class' in self.options:
-            node['classes'] = self.options['class']
+        node["code"] = "\n".join(self.content)
+        node["options"] = {"docname": self.env.docname}
+        if "alt" in self.options:
+            node["alt"] = self.options["alt"]
+        if "align" in self.options:
+            node["align"] = self.options["align"]
+        if "class" in self.options:
+            node["classes"] = self.options["class"]
 
-        if 'caption' not in self.options:
+        if "caption" not in self.options:
             self.add_name(node)
             return [node]
         else:
-            figure = figure_wrapper(self, node, self.options['caption'])
+            figure = figure_wrapper(self, node, self.options["caption"])
             self.add_name(figure)
             return [figure]
 
@@ -76,8 +77,9 @@ def html_visit_wavedrom(self: HTMLTranslator, node: wavedrom) -> None:
         # Wrap this in a ``data-component`` tag so that wavedrom code will be dynamically loaded. See ``webpack.index.js``, which references ``js/wavedrom.js``.
         '<div data-component="wavedrom">'
         # Finally, insert the WaveJSON file for this diagram.
-        '<script type="WaveDrom">{')
-    self.body.append(node['code'])
+        '<script type="WaveDrom">{'
+    )
+    self.body.append(node["code"])
     self.body.append("}</script></div>")
     # Skip the depart visitor -- the HTML above is all we need.
     raise nodes.SkipNode
