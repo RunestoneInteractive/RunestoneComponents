6065f07e3cd66cc84d224899ae7db1127f62c714
bjones1 <bjones@ece.msstate.edu>
2021-10-26 15:40:00 +0100

    Fix: correct failing test.

--- runestone/common/test/test_error/test_add_js_error.py
+++ runestone/common/test/test_error/test_add_js_error.py
@@ -1,3 +1,10 @@
+import pytest
+
+
+# This test causes Sphinx to produce an error, so don't abort the test when this happens. See `selenium_module_fixture`.
+pytestmark = pytest.mark.exit_status_success(False)
+
+
 def test_1(selenium_module_fixture):
     assert(
         "Extension error:\nUnable to find this_file_does_not_exist.js in html_static_path." in
