8f434982cf4e4935ba610a52116a01a16aa6b35a
bjones1 <bjones@ece.msstate.edu>
2022-03-22 21:04:03 -0500

    Fix: Remove incorrect comma. Black

--- runestone/common/test/test_error/test_add_js_error.py
+++ runestone/common/test/test_error/test_add_js_error.py
@@ -6,7 +6,7 @@ pytestmark = pytest.mark.exit_status_success(False)
 
 
 def test_1(selenium_module_fixture):
-    assert(
-        "Extension error:\nUnable to find this_file_does_not_exist.js in html_static_path." in
-        selenium_module_fixture.build_stderr_data,
+    assert (
+        "Extension error:\nUnable to find this_file_does_not_exist.js in html_static_path."
+        in selenium_module_fixture.build_stderr_data
     )
