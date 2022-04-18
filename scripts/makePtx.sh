d6abfbd6c305d77f5faaba76c7a30ee62e4d7467
Brad Miller <bonelake@mac.com>
2022-04-18 13:24:44 -0500

    update path

--- scripts/makePtx.sh
+++ scripts/makePtx.sh
@@ -8,7 +8,7 @@ fi
 source ~/.virtualenvs/compdev39/bin/activate
 npm run dist
 source ~/.virtualenvs/json2xml/bin/activate
-python dist2xml.py $1 > runestone/dist/webpack_static_imports.xml
+python scripts/dist2xml.py $1 > runestone/dist/webpack_static_imports.xml
 cd runestone
 tar zcf dist-$1.tgz dist
 deactivate
