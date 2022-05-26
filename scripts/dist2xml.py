ce1236eced073d59bb907f80d09ded401aa6ccb8
Brad Miller <bonelake@mac.com>
2022-05-26 13:21:24 -0500

    Fix: warn when env vars are not set

--- scripts/dist2xml.py
+++ scripts/dist2xml.py
@@ -7,7 +7,8 @@ import os
 if "COMPDIR" in os.environ:
     data_path = os.path.join(os.environ["COMPDIR"], "runestone/dist")
 else:
-    data_path = ""
+    print("Warning: No COMPDIR environment variable set using . for RunestoneComponents")
+    data_path = "runestone/dist"
 
 data = json.loads(
     open(os.path.join(data_path, "webpack_static_imports.json")).read())
@@ -15,6 +16,7 @@ data = json.loads(
 if sys.argv[1] == "test":
     data['cdn-url'] = sys.argv[2] if len(sys.argv) > 2 else "_static/"
 else:
+    print("Warning: You did not specify a URL for the cdn will use Runestone.Academy")
     data['cdn-url'] = "https://runestone.academy/cdn/runestone/"
 
 data['version'] = sys.argv[1]
