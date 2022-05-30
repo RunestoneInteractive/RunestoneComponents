0bb03a94d0c928218e476b1a31b98497709aa89f
Brad Miller <bonelake@mac.com>
2022-05-30 15:04:05 -0500

    Add usage

--- scripts/dist2xml.py
+++ scripts/dist2xml.py
@@ -4,6 +4,14 @@ from json2xml.utils import readfromstring
 import sys
 import os
 
+if len(sys.argv) < 2:
+    print("Usage: dist2xml.py version_number [cdn_url]")
+    print("    version_number should match the latest version of runestone components")
+    print("    cdn_url defaults to https://runestone.academy/cdn/runestone")
+    print("    The environment variable COMPDIR can be set to the root of RunestoneComponents")
+    print("    Otherwise you are best off running this command from the root of the RunestoneComponents distribution")
+    sys.exit(1)
+
 if "COMPDIR" in os.environ:
     data_path = os.path.join(os.environ["COMPDIR"], "runestone/dist")
 else:
