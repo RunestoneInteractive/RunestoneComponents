44ba9db3ea53cb1fd5fe2bca719ca830b8da147b
Brad Miller <bonelake@mac.com>
2022-09-24 08:14:16 -0500

    Moved PreTeXt sourc to new folder

--- makeRelease.sh
+++ makeRelease.sh
@@ -45,7 +45,7 @@ if [ -d ~/.virtualenvs/json2xml ]
     scp dist-$1.tgz balance.runestoneacademy.org:~/
     ssh balance.runestoneacademy.org /home/bmiller/bin/install_release.sh $1
     mv dist-$1.tgz ../jsdist
-    cp dist/webpack_static_imports.xml ~/src/pretext/xsl/support/runestone-services.xml 
+    cp dist/webpack_static_imports.xml ~/Pretext/pretext/xsl/support/runestone-services.xml 
     cp dist/webpack_static_imports.xml ~/.ptx/xsl/support/runestone-services.xml 
   else
     echo "Warning: no json2xml ve found skipping pretext"
